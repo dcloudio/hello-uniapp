143808ac1222aab9c252474b0da3d0c5fa9837c9
hbcui1984 <hbcui1984@163.com>
2018-11-19 18:33:50 +0800

    更改评论列表模板的文案

--- pages/template/comments/comments.vue
+++ pages/template/comments/comments.vue
@@ -8,13 +8,13 @@
 					<view class="uni-comment-face"><image src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png" mode="widthFix"></image></view>
 					<view class="uni-comment-body">
 						<view class="uni-comment-top">
-							<text>刘美丽</text>
+							<text>网友</text>
 							<text class="uni-iconfont icon-zan"></text>
 						</view>
 						<view class="uni-comment-date">
 							<text>08/10 08:12</text>
 						</view>
-						<view class="uni-comment-content">上天呀！我渴望与你相知相惜，长存此心永不褪减。</view>
+						<view class="uni-comment-content">很酷的HBuilderX和uni-app，开发一次既能生成小程序，又能生成App</view>
 					</view>
 				</view>
 				<view class="uni-comment-list">
@@ -23,7 +23,7 @@
 						<view class="uni-comment-top">
 							<text>马克一天</text>
 						</view>
-						<view class="uni-comment-content">除非巍巍群山消逝不见，除非滔滔江水干涸枯竭。除非凛凛寒冬雷声翻滚，除非炎炎酷暑白雪纷飞，除非天地相交聚合连接，直到这样的事情全都发生时，我才敢将对你的情意抛弃决绝！</view>
+						<view class="uni-comment-content">很强大，厉害了我的uni-app!</view>
 					</view>
 				</view>
 				<view class="uni-comment-list">
@@ -32,7 +32,7 @@
 						<view class="uni-comment-top">
 							<text>今生缘</text>
 						</view>
-						<view class="uni-comment-content">人面不知何处去，桃花依旧笑春风。</view>
+						<view class="uni-comment-content">好牛逼的感觉，是不是小程序、App、移动端都互通了？</view>
 						<view class="uni-comment-date">
 							<text>08/10 07:55</text>
 						</view>
@@ -44,7 +44,7 @@
 						<view class="uni-comment-top">
 							<text>小猫咪</text>
 						</view>
-						<view class="uni-comment-content">海上生明月，天涯共此时。。</view>
+						<view class="uni-comment-content">支持国产，支持DCloud!</view>
 						<view class="uni-comment-date">
 							<view>2天前</view><view class="uni-comment-replay-btn">5回复</view>
 						</view>
