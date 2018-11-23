e89bbf4e2292c65b7c0c2e0612a30ba925a9c951
hulinneil <1289739946@qq.com>
2018-11-23 12:22:24 +0800

    更新 label的样式

--- pages/component/label/label.vue
+++ pages/component/label/label.vue
@@ -4,52 +4,45 @@
 		<view class="uni-common-mt">
 			<view class="uni-form-item uni-column">
 				<view class="title">表单组件在label内</view>
-				<view class="uni-list">
-					<checkbox-group @change="checkboxChange">
-						<label class="uni-list-cell uni-list-cell-pd" v-for="item in checkboxItems" :key="item.name">
-							<view>
-								<checkbox :value="item.name" :checked="item.checked"></checkbox>
-							</view>
-							<view>{{item.value}}</view>
-						</label>
-					</checkbox-group>
-				</view>
+				<checkbox-group class="uni-list" @change="checkboxChange">
+					<label class="uni-list-cell uni-list-cell-pd" v-for="item in checkboxItems" :key="item.name">
+						<view>
+							<checkbox :value="item.name" :checked="item.checked"></checkbox>
+						</view>
+						<view>{{item.value}}</view>
+					</label>
+				</checkbox-group>
 			</view>
-			
+
 			<view class="uni-form-item uni-column">
 				<view class="title">label用for标识表单组件</view>
-				<view class="uni-list">
-					<radio-group @change="radioChange">
-						<label class="uni-list-cell uni-list-cell-pd" v-for="(item,index) in radioItems" :key="index">
-							<view>
-								<radio :id="item.name" :value="item.name" :checked="item.checked"></radio>
-							</view>
-							<view>
-								<label class="label-2-text" :for="item.name">
-									<text>{{item.value}}</text>
-								</label>
-							</view>
-						</label>
-					</radio-group>
-				</view>
-				
+				<radio-group class="uni-list" @change="radioChange">
+					<label class="uni-list-cell uni-list-cell-pd" v-for="(item,index) in radioItems" :key="index">
+						<view>
+							<radio :id="item.name" :value="item.name" :checked="item.checked"></radio>
+						</view>
+						<view>
+							<label class="label-2-text" :for="item.name">
+								<text>{{item.value}}</text>
+							</label>
+						</view>
+					</label>
+				</radio-group>
 			</view>
 
 			<view class="uni-form-item uni-column">
 				<view class="title">label内有多个时选中第一个</view>
-				<view class="uni-list">
-					<checkbox-group @change="checkboxChange">
-						<label class="label-3">
-							<view class="uni-list-cell uni-list-cell-pd">
-								<checkbox class="checkbox-3">选项一</checkbox>
-							</view>
-							<view class="uni-list-cell uni-list-cell-pd">
-								<checkbox class="checkbox-3">选项二</checkbox>
-							</view>
-							<view class="uni-link uni-center" style="margin-top:20upx;">点击该label下的文字默认选中第一个checkbox</view>
-						</label>
-					</checkbox-group>
-				</view>
+				<checkbox-group class="uni-list" @change="checkboxChange">
+					<label class="label-3">
+						<view class="uni-list-cell uni-list-cell-pd">
+							<checkbox class="checkbox-3">选项一</checkbox>
+						</view>
+						<view class="uni-list-cell uni-list-cell-pd">
+							<checkbox class="checkbox-3">选项二</checkbox>
+						</view>
+						<view class="uni-link uni-center" style="margin-top:20upx;">点击该label下的文字默认选中第一个checkbox</view>
+					</label>
+				</checkbox-group>
 			</view>
 
 		</view>
@@ -84,7 +77,8 @@
 			}
 		},
 		methods: {
-			checkboxChange: function (e) {
+			checkboxChange: function(e) {
+				console.log(e)
 				var checked = e.target.value
 				var changed = {}
 				for (var i = 0; i < this.checkboxItems.length; i++) {
@@ -95,7 +89,7 @@
 					}
 				}
 			},
-			radioChange: function (e) {
+			radioChange: function(e) {
 				var checked = e.target.value
 				var changed = {}
 				for (var i = 0; i < this.radioItems.length; i++) {
@@ -106,7 +100,7 @@
 					}
 				}
 			},
-			tapEvent: function (e) {
+			tapEvent: function(e) {
 				console.log('按钮被点击')
 			}
 		}
@@ -114,7 +108,11 @@
 </script>
 
 <style>
-.uni-list-cell {
-	justify-content: flex-start
-}
+	.uni-list-cell {
+		justify-content: flex-start
+	}
+
+	.uni-list .label-3 {
+		padding: 0;
+	}
 </style>
