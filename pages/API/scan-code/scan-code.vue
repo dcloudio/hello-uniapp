f1d399a120614505909cdaab4aefa00f2486c09b
xiaoyucoding <xiaoyucoding@126.com>
2018-12-17 19:46:04 +0800

    style: scanCode demo

--- pages/API/scan-code/scan-code.vue
+++ pages/API/scan-code/scan-code.vue
@@ -2,14 +2,16 @@
 	<view>
 		<page-head :title="title"></page-head>
 		<view class="uni-padding-wrap uni-common-mt">
-			<view class="uni-title">扫码结果</view>
-			<view class="uni-list">
+			<view class="uni-title">扫码结果：</view>
+			<view class="uni-list" v-if="result">
 				<view class="uni-cell">
-					<view class="uni-input">{{result}}</view>
+					<view class="scan-result">
+						{{result}}
+					</view>
 				</view>
 			</view>
 			<view class="uni-btn-v">
-				<button type="primary" @tap="scanCode">扫一扫</button>
+				<button type="primary" @click="scan">扫一扫</button>
 			</view>
 		</view>
 	</view>
@@ -22,23 +24,21 @@
 				result: ''
 			}
 		},
-		onUnload:function(){
-			this.result = '';
-		},
 		methods: {
-			scanCode: function () {
-				var that = this
+			scan: function() {
 				uni.scanCode({
-					success: function (res) {
-						that.result = res.result
-					},
-					fail: function (res) {}
-				})
+					success: (res) => {
+						this.result = res.result
+					}
+				});
 			}
 		}
 	}
 </script>
 
 <style>
-
+	.scan-result {
+		min-height: 50upx;
+		line-height: 50upx;
+	}
 </style>
