a46585f75b66c2b3354ffd61693fb539ea239b1a
xiaoyucoding <xiaoyucoding@126.com>
2018-12-23 16:52:29 +0800

    feat: 调整 ECharts 示例，支持在 H5 平台使用。

--- components/mpvue-echarts/src/echarts.vue
+++ components/mpvue-echarts/src/echarts.vue
@@ -45,7 +45,8 @@ export default {
       default: false,
     },
   },
-  onReady() {
+	// #ifdef H5
+  mounted() {
     if (!this.echarts) {
       console.warn('组件需绑定 echarts 变量，例：<ec-canvas id="mychart-dom-bar" '
         + 'canvas-id="mychart-bar" :echarts="echarts"></ec-canvas>');
@@ -54,18 +55,20 @@ export default {
 
     if (!this.lazyLoad) this.init();
   },
+	// #endif
+	// #ifndef H5
+	onReady() {
+		if (!this.echarts) {
+		  console.warn('组件需绑定 echarts 变量，例：<ec-canvas id="mychart-dom-bar" '
+		    + 'canvas-id="mychart-bar" :echarts="echarts"></ec-canvas>');
+		  return;
+		}
+		
+		if (!this.lazyLoad) this.init();
+	},
+	// #endif
   methods: {
     init() {
-      const version = wx.version.version.split('.').map(n => parseInt(n, 10));
-      const isValid = version[0] > 1 || (version[0] === 1 && version[1] > 9)
-        || (version[0] === 1 && version[1] === 9 && version[2] >= 91);
-      if (!isValid) {
-        console.error('微信基础库版本过低，需大于等于 1.9.91。'
-          + '参见：https://github.com/ecomfe/echarts-for-weixin'
-          + '#%E5%BE%AE%E4%BF%A1%E7%89%88%E6%9C%AC%E8%A6%81%E6%B1%82');
-        return;
-      }
-
       if (!this.onInit) {
         console.warn('请传入 onInit 函数进行初始化');
         return;
