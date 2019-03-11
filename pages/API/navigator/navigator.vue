99dd59a209359ec4b850040527dde7114040d07b
mehaotian <490272692@qq.com>
2019-03-11 15:13:49 +0800

    feat： 新增使用自定义动画打开页面示例

--- pages/API/navigator/navigator.vue
+++ pages/API/navigator/navigator.vue
@@ -8,6 +8,9 @@
 				<button @tap="redirectTo">在当前页面打开</button>
 				<button @tap="switchTab">切换到模板选项卡</button>
 				<button @tap="reLaunch">关闭所有页面，打开首页</button>
+				<!-- #ifdef APP-PLUS -->
+				<button @tap="customAnimation">使用自定义动画打开页面</button>
+				<!-- #endif -->
 			</view>
 		</view>
 	</view>
@@ -42,6 +45,13 @@
 				uni.reLaunch({
 					url: '/pages/tabBar/component/component'
 				});
+			},
+			customAnimation(){
+				uni.navigateTo({
+					url: 'new-page/new-page?data=使用自定义动画打开页面',
+					animationType: 'slide-in-bottom',
+					animationDuration: 200
+				})
 			}
 		}
 	}
