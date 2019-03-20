39853c41d9469fcde17b74533f7e6679e1a1aec7
xiaoyucoding <xiaoyucoding@126.com>
2019-03-20 20:30:11 +0800

    fix: 修复手势示例 key 冲突的问题

--- pages/template/gesture-lock/gesture-lock.vue
+++ pages/template/gesture-lock/gesture-lock.vue
@@ -11,36 +11,36 @@
 </template>
 
 <script>
-    import mpvueGestureLock from '@/components/mpvueGestureLock';
+	import mpvueGestureLock from '@/components/mpvueGestureLock/index.vue';
 
-    export default {
-        components: {
-            mpvueGestureLock
-        },
-        data() {
-            return {
-				title : "手势图案",
-                password: [],
-                text: '请设定手势'
-            }
-        },
-        methods: {
-            onEnd(data) {
-                if (this.password.length) {
-                    if (this.password.join('') === data.join('')) {
-                        this.text = '手势设定完成'
-                        this.password = []
-                    } else {
-                        this.text = '两次手势设定不一致'
-                        this.password = []
-                    }
-                } else {
-                    this.text = '请确认手势设定'
-                    this.password = data
-                }
-            }
-        }
-    }
+	export default {
+		components: {
+			mpvueGestureLock
+		},
+		data() {
+			return {
+				title: "手势图案",
+				password: [],
+				text: '请设定手势'
+			}
+		},
+		methods: {
+			onEnd(data) {
+				if (this.password.length) {
+					if (this.password.join('') === data.join('')) {
+						this.text = '手势设定完成'
+						this.password = []
+					} else {
+						this.text = '两次手势设定不一致'
+						this.password = []
+					}
+				} else {
+					this.text = '请确认手势设定'
+					this.password = data
+				}
+			}
+		}
+	}
 </script>
 
 <style>
