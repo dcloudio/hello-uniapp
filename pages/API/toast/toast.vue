49c3c60616a6949899ced99e3a077080776b4f7a
wanganxp <wanganxp@gmail.com>
2019-03-29 04:25:13 +0800

    给toast示例补充了一个app专用的居底toast

--- pages/API/toast/toast.vue
+++ pages/API/toast/toast.vue
@@ -7,6 +7,9 @@
 				<button type="default" @tap="toast2Tap">点击弹出设置duration的toast</button>
 				<button type="default" @tap="toast3Tap">点击弹出显示loading的toast</button>
 				<button type="default" @tap="toast4Tap">点击弹出显示自定义图片的toast</button>
+				<!-- #ifdef APP-PLUS -->
+				<button type="default" @tap="toast5Tap">点击显示无图标的居底toast</button>
+				<!-- #endif -->
 				<button type="default" @tap="hideToast">点击隐藏toast</button>
 			</view>
 		</view>
@@ -44,6 +47,11 @@
 					image: "../../../static/uni.png"
 				})
 			},
+			// #ifdef APP-PLUS
+			toast5Tap: function () {
+				plus.nativeUI.toast("显示一段轻提示")
+			},
+			// #endif
 			hideToast: function () {
 				uni.hideToast()
 			}
