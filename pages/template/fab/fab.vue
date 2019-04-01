fff098e574d1850c6122d4c483d6ccbd3ab38490
xiaoyucoding <xiaoyucoding@126.com>
2019-04-01 20:15:54 +0800

    update: 优化 fab 组件及示例

--- pages/template/fab/fab.vue
+++ pages/template/fab/fab.vue
@@ -26,92 +26,92 @@
 				右上角显示
 			</button>
 		</view>
-		<uni-fab
-			:pattern="pattern"
-			:content="content"
-			:horizontal="horizontal"
-			:vertical="vertical"
-			:direction="direction"
-			@trigger="trigger"
-		></uni-fab>
+		<uni-fab :pattern="pattern" :content="content" :horizontal="horizontal" :vertical="vertical" :direction="direction"
+		 @trigger="trigger" ref="fab"></uni-fab>
 	</view>
 </template>
 
 <script>
-import uniIcon from '@/components/uni-icon/uni-icon.vue';
-import uniFab from '@/components/uni-fab.vue';
-export default {
-	data() {
-		return {
-			title: 'uni-fab',
-			directionStr: '垂直',
-			horizontal: 'left',
-			vertical: 'bottom',
-			direction: 'horizontal',
-			pattern: {
-				color: '#7A7E83',
-				backgroundColor: '#fff',
-				selectedColor: '#007AFF',
-				buttonColor: '#007AFF'
-			},
-			content: [
-				{
-					iconPath: '/static/component.png',
-					selectedIconPath: '/static/componentHL.png',
-					text: '组件',
-					active: false
+	import uniIcon from '@/components/uni-icon/uni-icon.vue';
+	import uniFab from '@/components/uni-fab.vue';
+	export default {
+		data() {
+			return {
+				title: 'uni-fab',
+				directionStr: '垂直',
+				horizontal: 'left',
+				vertical: 'bottom',
+				direction: 'horizontal',
+				pattern: {
+					color: '#7A7E83',
+					backgroundColor: '#fff',
+					selectedColor: '#007AFF',
+					buttonColor: '#007AFF'
 				},
-				{
-					iconPath: '/static/api.png',
-					selectedIconPath: '/static/apiHL.png',
-					text: 'API',
-					active: false
-				},
-				{
-					iconPath: '/static/template.png',
-					selectedIconPath: '/static/templateHL.png',
-					text: '模版',
-					active: false
-				}
-			]
-		};
-	},
-	methods: {
-		trigger(e) {
-			console.log(e);
-			this.content[e.index].active = !e.item.active;
-			uni.showModal({
-				title: '提示',
-				content: `您${this.content[e.index].active ? '选中了' : '取消了'}${e.item.text}`,
-				success: function(res) {
-					if (res.confirm) {
-						console.log('用户点击确定');
-					} else if (res.cancel) {
-						console.log('用户点击取消');
+				content: [{
+						iconPath: '/static/component.png',
+						selectedIconPath: '/static/componentHL.png',
+						text: '组件',
+						active: false
+					},
+					{
+						iconPath: '/static/api.png',
+						selectedIconPath: '/static/apiHL.png',
+						text: 'API',
+						active: false
+					},
+					{
+						iconPath: '/static/template.png',
+						selectedIconPath: '/static/templateHL.png',
+						text: '模版',
+						active: false
 					}
-				}
-			});
+				]
+			};
 		},
-		switchBtn(hor, ver) {
-			if (hor === 0) {
-				this.direction = this.direction === 'horizontal' ? 'vertical' : 'horizontal';
-				this.directionStr = this.direction === 'horizontal' ? '垂直' : '水平';
-			} else {
-				this.horizontal = hor;
-				this.vertical = ver;
+		onBackPress() {
+			if (this.$refs.fab.isShow) {
+				this.$refs.fab.close();
+				return true;
 			}
-			this.$forceUpdate();
+			return false;
+		},
+		methods: {
+			trigger(e) {
+				console.log(e);
+				this.content[e.index].active = !e.item.active;
+				uni.showModal({
+					title: '提示',
+					content: `您${this.content[e.index].active ? '选中了' : '取消了'}${e.item.text}`,
+					success: function(res) {
+						if (res.confirm) {
+							console.log('用户点击确定');
+						} else if (res.cancel) {
+							console.log('用户点击取消');
+						}
+					}
+				});
+			},
+			switchBtn(hor, ver) {
+				if (hor === 0) {
+					this.direction = this.direction === 'horizontal' ? 'vertical' : 'horizontal';
+					this.directionStr = this.direction === 'horizontal' ? '垂直' : '水平';
+				} else {
+					this.horizontal = hor;
+					this.vertical = ver;
+				}
+				this.$forceUpdate();
+			}
+		},
+		components: {
+			uniFab,
+			uniIcon
 		}
-	},
-	components: {
-		uniFab,
-		uniIcon
-	}
-};
+	};
 </script>
 
 <style>
-.btn {
-	margin: 30upx 0;
-}
+	.btn {
+		margin: 30upx 0;
+	}
 </style>
