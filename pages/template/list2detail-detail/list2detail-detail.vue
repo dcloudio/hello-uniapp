d8214b05814d58146b6981644aa6ae31a78878dd
xiaoyucoding <xiaoyucoding@126.com>
2019-04-04 16:42:04 +0800

    feat: 列表到详情模板在 App 端支持分享到微信

--- pages/template/list2detail-detail/list2detail-detail.vue
+++ pages/template/list2detail-detail/list2detail-detail.vue
@@ -19,32 +19,102 @@
 </template>
 
 <script>
+	const DETAIL_PAGE_PATH = '/pages/template/list2detail-detail/list2detail-detail';
+
+	function _handleShareChannels(provider) {
+		let channels = [];
+		for (let i = 0, len = provider.length; i < len; i++) {
+			switch (provider[i]) {
+				case 'weixin':
+					channels.push({
+						text: '分享到微信好友',
+						id: 'weixin',
+						sort: 0
+					});
+					channels.push({
+						text: '分享到微信朋友圈',
+						id: 'weixin',
+						sort: 1
+					});
+					break;
+				default:
+					break;
+			}
+		}
+		channels.sort(function(x, y) {
+			return x.sort - y.sort;
+		});
+		return channels;
+	}
+
 	export default {
 		data() {
 			return {
 				title: 'list-triplex-row',
 				banner: {},
-				htmlString: ""
+				htmlString: ''
 			}
+		},
+		onLoad(event) {
+			// TODO 后面把参数名替换成 payload
+			const payload = event.detailDate || event.payload;
+			// 目前在某些平台参数会被主动 decode，暂时这样处理。
+			try {
+				this.banner = JSON.parse(decodeURIComponent(payload));
+			} catch (error) {
+				this.banner = JSON.parse(payload);
+			}
+			uni.setNavigationBarTitle({
+				title: this.banner.title
+			});
+			this.getDetail();
 		},
 		onShareAppMessage() {
 			return {
 				title: this.banner.title,
-				path: '/pages/template/list2detail-detail/list2detail-detail?detailDate=' + JSON.stringify(this.banner)
+				path: DETAIL_PAGE_PATH + '?detailDate=' + JSON.stringify(this.banner)
 			}
 		},
-		onLoad(event) {
-			// 目前在某些平台参数会被主动 decode，暂时这样处理。
-			try {
-				this.banner = JSON.parse(decodeURIComponent(event.detailDate));
-			} catch (error) {
-				this.banner = JSON.parse(event.detailDate);
+		onNavigationBarButtonTap(event) {
+			const buttonIndex = event.index;
+			if (buttonIndex === 0) {
+				// 分享 H5 的页面
+				const shareProviders = [];
+				uni.getProvider({
+					service: 'share',
+					success: (result) => {
+						// 目前仅考虑分享到微信
+						if (result.provider && result.provider.length && ~result.provider.indexOf('weixin')) {
+							const channels = _handleShareChannels(result.provider);
+							uni.showActionSheet({
+								itemList: channels.map(channel => {
+									return channel.text;
+								}),
+								success: (result) => {
+									const tapIndex = result.tapIndex;
+									uni.share({
+										provider: 'weixin',
+										type: 0,
+										title: this.banner.title,
+										scene: tapIndex === 0 ? 'WXSceneSession' : 'WXSenceTimeline',
+										href: 'https://uniapp.dcloud.io/h5' + DETAIL_PAGE_PATH + '?detailDate=' + JSON.stringify(this.banner),
+										imageUrl: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/app/share-logo@3.png'
+									});
+								}
+							});
+						} else {
+							uni.showToast({
+								title: '未检测到可用的微信分享服务'
+							});
+						}
+					},
+					fail: (error) => {
+						uni.showToast({
+							title: '获取分享服务失败'
+						});
+					}
+				});
 			}
-
-			this.getDetail();
-			uni.setNavigationBarTitle({
-				title: this.banner.title
-			});
 		},
 		methods: {
 			getDetail() {
@@ -58,7 +128,7 @@
 					fail: () => {
 						console.log('fail');
 					}
-				})
+				});
 			}
 		}
 	}
