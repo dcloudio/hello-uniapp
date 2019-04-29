7adec139a8151ba39d5202eb14bbaf458bb67180
mehaotian <490272692@qq.com>
2019-04-29 10:14:10 +0800

    feat: hello uni-app 新增 指纹示例 , SQLite 示例

---
+++ pages/API/fingerprint/fingerprint.vue
@@ -0,0 +1,98 @@
+<template>
+	<view>
+		<page-head :title="title"></page-head>
+		<view class="uni-padding-wrap uni-common-mt">
+			<view class="uni-btn-v"><button type="primary" @tap="fingerprint()" :disabled="disabled">按下开始识别指纹</button></view>
+			<view style="width: 100%;text-align: center;">{{ result }}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: '指纹',
+			result: '',
+			disabled: true
+		};
+	},
+	onLoad() {
+		// #ifdef APP-PLUS
+		if (!plus.fingerprint.isSupport()) {
+			this.result = '此设备不支持指纹识别';
+			this.disabled = true;
+		} else if (!plus.fingerprint.isKeyguardSecure()) {
+			this.result = '此设备未设置密码锁屏，无法使用指纹识别';
+			this.disabled = true;
+		} else if (!plus.fingerprint.isEnrolledFingerprints()) {
+			this.result = '此设备未录入指纹，请到设置中开启';
+			this.disabled = true;
+		} else {
+			this.result = '此设备支持指纹识别';
+			this.disabled = false;
+		}
+		// #endif
+		// #ifdef MP-WEIXIN
+		this.disabled = false;
+		this.result = '请在微信真机中使用，模拟器不支持';
+		// #endif
+		// #ifndef APP-PLUS || MP-WEIXIN
+		this.result = '此平台不支持指纹识别';
+		// #endif
+	},
+	methods: {
+		fingerprint: function() {
+			// #ifdef APP-PLUS
+			plus.fingerprint.authenticate(
+				function() {
+					plus.nativeUI.closeWaiting(); //兼容Android平台关闭等待框
+					plus.nativeUI.alert('指纹识别成功');
+				},
+				function(e) {
+					switch (e.code) {
+						case e.AUTHENTICATE_MISMATCH:
+							plus.nativeUI.toast('指纹匹配失败，请重新输入');
+							break;
+						case e.AUTHENTICATE_OVERLIMIT:
+							plus.nativeUI.closeWaiting(); //兼容Android平台关闭等待框
+							plus.nativeUI.alert('指纹识别失败次数超出限制，请使用其它方式进行认证');
+							break;
+						case e.CANCEL:
+							plus.nativeUI.toast('已取消识别');
+							break;
+						default:
+							plus.nativeUI.closeWaiting(); //兼容Android平台关闭等待框
+							plus.nativeUI.alert('指纹识别失败，请重试');
+							break;
+					}
+				}
+			);
+			// Android平台手动弹出等待提示框
+			if ('Android' == plus.os.name) {
+				plus.nativeUI.showWaiting('指纹识别中...').onclose = function() {
+					plus.fingerprint.cancel();
+				};
+			}
+			// #endif
+
+			// #ifdef MP-WEIXIN
+			wx.startSoterAuthentication({
+				requestAuthModes: ['fingerPrint'],
+				challenge: '123456',
+				authContent: '请用指纹解锁',
+				success(res) {
+					uni.showToast({
+						title: '识别成功',
+						mask: false,
+						duration: 1500
+					});
+				}
+			});
+			// #endif
+		}
+	}
+};
+</script>
+
+<style></style>
---
+++ pages/API/sqlite/sqlite.vue
@@ -0,0 +1,120 @@
+<template>
+	<view>
+		<page-head :title="title"></page-head>
+		<view class="uni-padding-wrap uni-common-mt">
+			<view class="uni-btn-v"><button type="primary" @click="openDB">打开数据库test.db</button></view>
+			<view class="uni-btn-v"><button type="primary" @click="executeSQL">创建表database及插入数据</button></view>
+			<view class="uni-btn-v"><button type="primary" @click="selectSQL">查询表database的数据</button></view>
+			<view class="uni-btn-v"><button type="primary" @click="droptable">删除表database</button></view>
+			<view class="uni-btn-v"><button type="primary" @click="closeDB">关闭数据库test.db</button></view>
+			<view class="uni-btn-v"><button type="primary" @click="isOpenDB">查询是否打开数据库</button></view>
+			
+			
+			
+			
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'SQLite'
+		};
+	},
+	methods: {
+		openDB: function() {
+			plus.sqlite.openDatabase({
+				name: 'first',
+				path: '_doc/test.db',
+				success: function(e) {
+					plus.nativeUI.alert('打开数据库test.db: ' + JSON.stringify(e));
+				},
+				fail: function(e) {
+					plus.nativeUI.alert('打开数据库test.db: ' + JSON.stringify(e));
+				}
+			});
+		},
+		// 执行SQL语句
+		executeSQL: function() {
+			plus.sqlite.executeSql({
+				name: 'first',
+				sql: 'create table if not exists database("name" CHAR(110),"sex" CHAR(10),"age" INT(11))',
+				success: function(e) {
+					plus.sqlite.executeSql({
+						name: 'first',
+						sql: "insert into database values('彦','女','7000')",
+						success: function(e) {
+							plus.nativeUI.alert('执行SQL语句: ' + JSON.stringify(e));
+						},
+						fail: function(e) {
+							plus.nativeUI.alert('执行SQL语句: ' + JSON.stringify(e));
+						}
+					});
+				},
+				fail: function(e) {
+					plus.nativeUI.alert('执行SQL语句: ' + JSON.stringify(e));
+				}
+			});
+		},
+		// 查询SQL语句
+		selectSQL: function() {
+			plus.sqlite.selectSql({
+				name: 'first',
+				sql: 'select * from database',
+				success: function(e) {
+					plus.nativeUI.alert('查询SQL语句: ' + JSON.stringify(e));
+				},
+				fail: function(e) {
+					plus.nativeUI.alert('查询SQL语句: ' + JSON.stringify(e));
+				}
+			});
+		},
+		// 删除表
+		droptable: function() {
+			plus.sqlite.executeSql({
+				name: 'first',
+				sql: 'drop table database',
+				success: function(e) {
+					plus.nativeUI.alert('删除表database: ' + JSON.stringify(e));
+				},
+				fail: function(e) {
+					plus.nativeUI.alert('删除表database: ' + JSON.stringify(e));
+				}
+			});
+		},
+		// 关闭数据库
+		closeDB: function() {
+			plus.sqlite.closeDatabase({
+				name: 'first',
+				success: function(e) {
+					plus.nativeUI.alert('关闭数据库: ' + JSON.stringify(e));
+				},
+				fail: function(e) {
+					plus.nativeUI.alert('关闭数据库: ' + JSON.stringify(e));
+				}
+			});
+		},
+		isOpenDB: function() {
+			if (
+				plus.sqlite.isOpenDatabase({
+					name: 'first',
+					path: '_doc/test.db'
+				})
+			) {
+				plus.nativeUI.alert('Opened!');
+			} else {
+				plus.nativeUI.alert('Unopened!');
+			}
+		}
+	}
+};
+</script>
+
+<style>
+	.uni-btn-v {
+		margin: 20upx 0;
+		padding: 0;
+	}
+</style>
