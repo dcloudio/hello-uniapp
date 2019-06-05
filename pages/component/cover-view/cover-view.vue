89149974783ff88286fb724ca05c3cee4248aabb
handongxun <handongxun@dcloud.io>
2019-06-05 18:32:37 +0800

    fix: 完善上次提交 未彻底解决支付宝真机地图高度及位置不正确问题

--- pages/component/cover-view/cover-view.vue
+++ pages/component/cover-view/cover-view.vue
@@ -1,21 +1,19 @@
 <template>
 	<view>
 		<page-head title="cover-view用于覆盖map、video等原生组件"></page-head>
-		<view class="uni-common-mt">
-			<view class="cover-content">
-				<!-- TODO暂时条件编译解决支付宝小程序不能正常显示 cover-x -->
-				<!-- #ifdef APP-PLUS -->
-				<map>
-					<cover-view class="cover-view">简单的cover-view</cover-view>
-					<cover-image class="cover-image" src="/static/uni.png"></cover-image>
-				</map>
-				<!-- #endif -->
-				<!-- #ifndef APP-PLUS -->
-				<map></map>
+		<view class="cover-content">
+			<!-- TODO暂时条件编译解决支付宝小程序不能正常显示 cover-x -->
+			<!-- #ifdef APP-PLUS -->
+			<map>
 				<cover-view class="cover-view">简单的cover-view</cover-view>
 				<cover-image class="cover-image" src="/static/uni.png"></cover-image>
-				<!-- #endif -->
-			</view>
+			</map>
+			<!-- #endif -->
+			<!-- #ifndef APP-PLUS -->
+			<map></map>
+			<cover-view class="cover-view">简单的cover-view</cover-view>
+			<cover-image class="cover-image" src="/static/uni.png"></cover-image>
+			<!-- #endif -->
 		</view>
 	</view>
 </template>
@@ -31,6 +29,7 @@
 <style>
 	map {
 		width: 750upx;
+		height: 750upx;
 	}
 
 	.cover-content {
