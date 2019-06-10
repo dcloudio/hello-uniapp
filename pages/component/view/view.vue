1b0d63ff855a7611e221d52ae52a6351225173e7
qiang <guoshengqiang@live.com>
2019-06-10 15:51:02 +0800

    调整view示例样式，适配iOS8

--- pages/component/view/view.vue
+++ pages/component/view/view.vue
@@ -3,9 +3,10 @@
 		<page-head title="view"></page-head>
 		<view class="uni-padding-wrap uni-common-mt">
 			<view class="uni-hello-text">
-				Flex是Flexible Box的缩写，意为“弹性布局”，用来为盒状模型提供最大的灵活性。当设置display: flex后，继续给view等容器组件设置flex-direction: row或column，就可以在该容器内按行或列排布子组件。uni-app默认全局使用flex布局(在App.vue里预设了)。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。
+				Flex是Flexible Box的缩写，意为“弹性布局”，用来为盒状模型提供最大的灵活性。当设置display: flex后，继续给view等容器组件设置flex-direction:
+				row或column，就可以在该容器内按行或列排布子组件。uni-app默认全局使用flex布局(在App.vue里预设了)。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。
 			</view>
-			
+
 			<view class="uni-title uni-common-mt">
 				flex-direction: row
 				<text>\n横向布局</text>
@@ -15,7 +16,7 @@
 				<view class="flex-item uni-bg-green">B</view>
 				<view class="flex-item uni-bg-blue">C</view>
 			</view>
-			
+
 			<view class="uni-title uni-common-mt">
 				flex-direction: column
 				<text>\n纵向布局</text>
@@ -25,7 +26,7 @@
 				<view class="flex-item flex-item-V uni-bg-green">B</view>
 				<view class="flex-item flex-item-V uni-bg-blue">C</view>
 			</view>
-			
+
 			<view class="uni-title uni-common-mt">
 				更多布局示例
 				<text>\nflex布局演示</text>
@@ -37,29 +38,29 @@
 					<view class="text">横向布局-自动宽度</view>
 					<view class="text">横向布局-自动宽度</view>
 				</view>
-				<view class="uni-flex uni-row" style="justify-content: center;">
+				<view class="uni-flex uni-row" style="-webkit-justify-content: center;justify-content: center;">
 					<view class="text">横向布局-居中</view>
 					<view class="text">横向布局-居中</view>
 				</view>
-				<view class="uni-flex uni-row" style="justify-content: flex-end;">
+				<view class="uni-flex uni-row" style="-webkit-justify-content: flex-end;justify-content: flex-end;">
 					<view class="text">横向布局-居右</view>
 					<view class="text">横向布局-居右</view>
 				</view>
 				<view class="uni-flex uni-row">
-					<view class="text" style="flex: 1;">横向布局-平均分布</view>
-					<view class="text" style="flex: 1;">横向布局-平均分布</view>
+					<view class="text" style="-webkit-flex: 1;flex: 1;">横向布局-平均分布</view>
+					<view class="text" style="-webkit-flex: 1;flex: 1;">横向布局-平均分布</view>
 				</view>
-				<view class="uni-flex uni-row" style="justify-content: space-between;">
+				<view class="uni-flex uni-row" style="-webkit-justify-content: space-between;justify-content: space-between;">
 					<view class="text">横向布局-两端对齐</view>
 					<view class="text">横向布局-两端对齐</view>
 				</view>
 				<view class="uni-flex uni-row">
 					<view class="text" style="width: 200upx;">固定宽度</view>
-					<view class="text" style="flex: 1;">自动占满余量</view>
+					<view class="text" style="-webkit-flex: 1;flex: 1;">自动占满余量</view>
 				</view>
 				<view class="uni-flex uni-row">
 					<view class="text" style="width: 200upx;">固定宽度</view>
-					<view class="text" style="flex: 1;">自动占满</view>
+					<view class="text" style="-webkit-flex: 1;flex: 1;">自动占满</view>
 					<view class="text" style="width: 200upx;">固定宽度</view>
 				</view>
 				<view class="uni-flex uni-row" style="-webkit-flex-wrap: wrap;flex-wrap: wrap;">
@@ -68,33 +69,33 @@
 					<view class="text" style="width: 280upx;">一行显示不全,wrap折行</view>
 				</view>
 				<view class="uni-flex uni-row">
-					<view class="text" style="flex: 1;height: 200upx;display: flex; justify-content: center;align-items: flex-start;">
+					<view class="text uni-flex" style="-webkit-flex: 1;flex: 1;height: 200upx;-webkit-justify-content: center;justify-content: center;-webkit-align-items: flex-start;align-items: flex-start;">
 						<text>垂直居顶</text>
 					</view>
-					<view class="text" style="flex: 1;height: 200upx;display: flex; justify-content: center;align-items: center;">
+					<view class="text uni-flex" style="-webkit-flex: 1;flex: 1;height: 200upx;-webkit-justify-content: center;justify-content: center;-webkit-align-items: center;align-items: center;">
 						<text>垂直居中</text>
 					</view>
-					<view class="text" style="flex: 1;height: 200upx;display: flex; justify-content: center;align-items: flex-end;">
+					<view class="text uni-flex" style="-webkit-flex: 1;flex: 1;height: 200upx;-webkit-justify-content: center;justify-content: center;-webkit-align-items: flex-end;align-items: flex-end;">
 						<text>垂直居底</text>
 					</view>
 				</view>
 			</view>
-			
+
 			<view class="uni-title uni-common-mt">
 				组合示例
 				<text>\nflex布局演示</text>
 			</view>
 			<view class="uni-flex uni-row">
-				<view class="text" style="width: 200upx;height: 220upx;display: flex; justify-content: center;align-items: center;">
+				<view class="text" style="width: 200upx;height: 220upx;display: -webkit-flex;display: flex; -webkit-justify-content: center;justify-content: center;-webkit-align-items: center;align-items: center;">
 					<image src="../../../static/plus.png" style="width: 150upx;height: 150upx;"></image>
 				</view>
-				<view class="uni-flex uni-column" style="flex: 1;justify-content: space-between;">
+				<view class="uni-flex uni-column" style="-webkit-flex: 1;flex: 1;-webkit-justify-content: space-between;justify-content: space-between;">
 					<view class="text" style="height: 120upx;text-align: left;padding-left: 20upx;padding-top: 10upx;">
 						文字居左，留出左间距
 					</view>
 					<view class="uni-flex uni-row">
-						<view class="text" style="flex: 1;">剩余数量</view>
-						<view class="text" style="flex: 1;">立即购买</view>
+						<view class="text" style="-webkit-flex: 1;flex: 1;">剩余数量</view>
+						<view class="text" style="-webkit-flex: 1;flex: 1;">立即购买</view>
 					</view>
 				</view>
 			</view>
@@ -104,8 +105,7 @@
 <script>
 	export default {
 		data() {
-			return {
-			}
+			return {}
 		}
 	}
 </script>
@@ -124,8 +124,9 @@
 		text-align: center;
 		line-height: 150upx;
 	}
-	.text{
-		margin:15upx 10upx;
+
+	.text {
+		margin: 15upx 10upx;
 		padding: 0 20upx;
 		background-color: #ebebeb;
 		height: 70upx;
@@ -134,7 +135,8 @@
 		color: #777;
 		font-size: 26upx;
 	}
-	.desc{
+
+	.desc {
 		/* text-indent: 40upx; */
 	}
 </style>
