fd43606d60772410a9a83bb157e79b5850ed4e23
wangyaqi <wangyaqi988@163.com>
2019-10-22 15:29:39 +0800

    update: 增加组件间通讯示例

---
+++ pages/template/component-communication/component-communication.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+        <page-head title="组件通讯示例"></page-head>
+        <view class="uni-padding-wrap">
+            <reciver></reciver>
+            <sender></sender>
+            <sender-bus></sender-bus>
+        </view>
+	</view>
+</template>
+
+<script>
+    import reciver from './reciver.vue'
+    import sender from './sender.vue'
+    import senderBus from './sender-bus.vue'
+	export default {
+        components:{
+           reciver,
+           sender,
+           senderBus
+        },
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+    .uni-padding-wrap{
+        width: auto;
+        padding: 20px 30px;
+    }
+</style>
---
+++ pages/template/component-communication/sender.vue
@@ -0,0 +1,24 @@
+<template>
+    <view class="sender-container">
+        <button type="primary" @click="send">点击发送消息</button>
+    </view>
+</template>
+
+<script>
+    export default {
+        methods: {
+            send() {
+                let num = parseInt(Math.random() * 10000)
+                uni.$emit('cc', {
+                    msg: 'From uni.$emit -> ' + num
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    .sender-container{
+        padding: 20px;
+    }
+</style>
