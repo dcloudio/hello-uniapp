6d0317d5b2586fbdae57b2ba65efff69ebc1fc28
wangyaqi <wangyaqi988@163.com>
2019-12-02 17:09:56 +0800

    update: websocket url

--- pages/API/websocket-global/websocket-global.vue
+++ pages/API/websocket-global/websocket-global.vue
@@ -62,7 +62,7 @@
 					title: '连接中...'
 				})
 				uni.connectSocket({
-					url: 'wss://connect.websocket.in/hello_uni_app?room_id=' + this.roomId,
+					url: 'wss://connect.websocket.in/v2/'+this.roomId+'?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU3Y2EzYTQyOTc3ODZjMjE1NTdkYmJmOGZmZmU5YzRhMWFmZTViM2I3NTcxZTNiNGE4OGU2ZDFiNWYxNTJiNWUzZTAyNTk3YzFlMDkwMjE0In0.eyJhdWQiOiI2IiwianRpIjoiZTdjYTNhNDI5Nzc4NmMyMTU1N2RiYmY4ZmZmZTljNGExYWZlNWIzYjc1NzFlM2I0YTg4ZTZkMWI1ZjE1MmI1ZTNlMDI1OTdjMWUwOTAyMTQiLCJpYXQiOjE1NzUyNzczMjcsIm5iZiI6MTU3NTI3NzMyNywiZXhwIjoxNjA2ODk5NzI3LCJzdWIiOiIxMTEiLCJzY29wZXMiOltdfQ.vqjnvM1xwolhlAtJfhWFjckJ7QDjc3bU0eONChzK0MCzjs-zQg2C8uq_3Gm5aPDYpFd0nRONRwAp96LxtwFvExlFLzm0ND1g-v7tqrgcqMdUqpczQNqQ-qmx5SoAwXZ8TLzw1HSQtG7eoY8Q7y00qr46-nX-dQyH-LrHw6E32dFlLXND6Srjlo6hl_Zy1XMaOA8Jf64i2uQOLWLjdJDeQedyqMZVZS34-9h81Ri3DPM3C1UkUQAnwJUwRbcbGS49i8VJNBRHLd-9FkrrCNWB-dDLyZnwrBpcStEpXclGbwLv1NtKRYhW0E7fNCf-nq945JEdT1EB9EpyQOb5uV2iLlsIgtmvBn1aaKvL6fWaFq04T09BXSsXuCewoU8XLpY54SSu3r5IuaG8eR_aClCkylRZKMyfRZFas6lWbExt7-F56e_ZXuK6i50OZ0NyFiDkl6qqiwqaF7K-oj-Q8GhCcEN5jkOV6qfWbxlT_3D7L1nvDgrDENjOlfgjcacB3rSsc4ftqx-GJmY5QRh4GE4a07Il1xtEzGm49Ki4vZeYjZsc66E9Iqgw7lINm8TppxPlgY_Wc7wdUXQ959wTyTmiEwpMqxIKn7Cfelm_fZ6HKaJqZ3ZRfuKUEXZVWG2o3UW7cpmiz7TJ3-spVelck6iKfLlrZ9qCzX7aKnspiTWHThQ',
 					data() {
 						return {
 							msg: 'Hello'
