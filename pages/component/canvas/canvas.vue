39e6b9c6b63971969c2d736164ac1f65678a941d
wangyaqi <wangyaqi988@163.com>
2019-12-09 18:44:36 +0800

    update: 新增canvas组件示例

--- pages/component/canvas/canvas.vue
+++ pages/component/canvas/canvas.vue
@@ -1,96 +1,156 @@
 <template>
-	<view>
-		<page-head :title="title"></page-head>
-		<view class="page-body">
-			<view class="page-body-wrapper">
-				<canvas canvas-id="canvas" class="canvas"></canvas>
-			</view>
-		</view>
-	</view>
+    <view>
+        <page-head :title="title"></page-head>
+        <view class="page-body">
+            <view class="page-body-wrapper">
+                <!-- #ifdef APP-PLUS || H5 -->
+                <canvas canvas-id="canvas" class="canvas" :start="start" :change:start="animate.start" :data-width="canvasWidth"
+                    :data-height="canvasWidth"></canvas>
+                <!-- #endif -->
+                <!-- #ifndef APP-PLUS || H5 -->
+                <canvas canvas-id="canvas" id="canvas" class="canvas"></canvas>
+                <!-- #endif -->
+            </view>
+        </view>
+    </view>
 </template>
+
+<script module="animate" lang="wxs" src="./animate.wxs"></script>
+
 <script>
-	var context = null,interval = null;
-	
-	export default {
-		data() {
-			return {
-				title: 'canvas',
-				screenWidth:uni.getSystemInfoSync().windowWidth,
-				canvasWidth:0,
-				position: {
-					x: 150,
-					y: 150,
-					vx: 2,
-					vy: 2
-				}
-			}
-		},
-		onReady: function () {
-			context = uni.createCanvasContext('canvas');
-			this.canvasWidth = this.screenWidth / 750 * 610;
-			this.position = {
-				x:this.canvasWidth/2,
-				y:this.canvasWidth/2,
-				vx:2,
-				vy:2
-			}
-			this.drawBall();
-			interval = setInterval(this.drawBall, 17)
-		},
-		onUnload: function () {
-			clearInterval(interval);
-			this.position = {
-				x: 0,
-				y: 0,
-				vx: 0,
-				vy: 0
-			}
-		},
-		methods: {
-			drawBall: function () {
-				var p = this.position
-				p.x += p.vx
-				p.y += p.vy
-				if (p.x >= this.canvasWidth) {
-					p.vx = -2
-				}
-				if (p.x <= 7) {
-					p.vx = 2
-				}
-				if (p.y >= this.canvasWidth) {
-					p.vy = -2
-				}
-				if (p.y <= 7) {
-					p.vy = 2
-				}
-				function ball(x, y) {
-					context.beginPath(0)
-					context.arc(x, y, 5, 0, Math.PI * 2)
-					context.setFillStyle('#1aad19')
-					context.setStrokeStyle('rgba(1,1,1,0)')
-					context.fill()
-					context.stroke()
-				}
+    // #ifndef APP-PLUS || H5
 
-				ball(p.x, this.canvasWidth/2)
-				ball(this.canvasWidth/2, p.y)
-				ball(this.canvasWidth - p.x, this.canvasWidth/2)
-				ball(this.canvasWidth/2, this.canvasWidth - p.y)
-				ball(p.x, p.y)
-				ball(this.canvasWidth - p.x, this.canvasWidth - p.y)
-				ball(p.x, this.canvasWidth - p.y)
-				ball(this.canvasWidth - p.x, p.y)
+    var ctx = null,
+        interval = null;
 
-				context.draw()
-			}
-		}
-	}
+    function Ball(x, y, vx, vy, canvasWidth, canvasHeight, ctx) {
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
+        this.ctx = ctx
+        this.x = x
+        this.y = y
+        this.vx = vx
+        this.vy = vy
+        this.radius = 5
+    }
+
+    Ball.prototype.draw = function() {
+        this.ctx.setFillStyle('#007AFF')
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        this.ctx.closePath()
+        this.ctx.fill()
+    }
+
+    Ball.prototype.move = function() {
+        this.x += this.vx
+        this.y += this.vy
+        // 回到中心
+        // if (getDistance(this.x - this.canvasWidth / 2, this.y - this.canvasHeight / 2) >
+        // 	getDistance(this.canvasWidth / 2, this.canvasHeight / 2) + this.radius) {
+        // 	this.x = this.canvasWidth / 2
+        // 	this.y = this.canvasHeight / 2
+        // }
+
+        // 边框反弹
+        if (this.x < this.radius) {
+            this.vx = Math.abs(this.vx)
+            return
+        }
+        if (this.x > this.canvasWidth - this.radius) {
+            this.vx = -Math.abs(this.vx)
+        }
+        if (this.y < this.radius) {
+            this.vy = Math.abs(this.vy)
+            return
+        }
+        if (this.y > this.canvasWidth - this.radius) {
+            this.vy = -Math.abs(this.vy)
+        }
+    }
+
+    function getDistance(x, y) {
+        return Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5)
+    }
+
+    // #endif
+
+    export default {
+        data() {
+            return {
+                title: 'canvas',
+                canvasWidth: 0,
+                start: false,
+                ballList: []
+            }
+        },
+        onReady: function() {
+            this.$nextTick(() => {
+                uni.createSelectorQuery().select(".canvas").boundingClientRect(data => {
+                    this.canvasWidth = data.width
+                    // #ifdef APP-PLUS || H5
+                    this.start = true
+                    // #endif
+                    // #ifndef APP-PLUS || H5
+                    ctx = uni.createCanvasContext('canvas')
+                    this.drawBall()
+                    // #endif
+                }).exec()
+            })
+        },
+        // #ifndef APP-PLUS || H5
+        onUnload: function() {
+            clearInterval(interval);
+        },
+        methods: {
+            drawBall: function() {
+                var canvasWidth = this.canvasWidth,
+                    canvasHeight = this.canvasWidth,
+                    speed = 3,
+                    ballList = [],
+                    layer = 3,
+                    ballInlayer = 20
+                for (var i = 0; i < layer; i++) {
+                    var radius = getDistance(canvasWidth / 2, canvasHeight / 2) / layer * i
+                    for (var j = 0; j < ballInlayer; j++) {
+                        var deg = j * 2 * Math.PI / ballInlayer,
+                            sin = Math.sin(deg),
+                            cos = Math.cos(deg),
+                            x = radius * cos + canvasWidth / 2,
+                            y = radius * sin + canvasHeight / 2,
+                            vx = speed * cos,
+                            vy = speed * sin
+                        ballList.push(new Ball(x, y, vx, vy, canvasWidth, canvasHeight, ctx))
+                    }
+                }
+
+                function animate(ballList) {
+                    // ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+                    ballList.forEach(function(item) {
+                        item.move()
+                        item.draw()
+                    })
+                    ctx.draw()
+                }
+
+                interval = setInterval(function() {
+                    animate(ballList)
+                }, 17)
+            }
+        }
+        // #endif
+    }
 </script>
 
 <style>
-	.canvas {
-		width: 610upx;
-		height: 610upx;
-		background-color: #fff;
-	}
+    .page-body-wrapper {
+        text-align: center;
+    }
+
+    .canvas {
+        width: 610rpx;
+        height: 610rpx;
+        margin: auto;
+        background-color: #fff;
+    }
 </style>
