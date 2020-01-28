72ff6cb527fae7b54e2b6237522192b1a13adb61
wangyaqi <wangyaqi988@163.com>
2020-01-29 00:01:25 +0800

    update(uni-ui): onlyPlusButton --> popMenu

--- components/uni-fab/uni-fab.vue
+++ components/uni-fab/uni-fab.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<view v-if="!onlyPlusButton && (leftBottom||rightBottom||leftTop||rightTop)" :class="{
+		<view v-if="popMenu && (leftBottom||rightBottom||leftTop||rightTop)" :class="{
         'uni-fab--leftBottom': leftBottom,
         'uni-fab--rightBottom': rightBottom,
         'uni-fab--leftTop': leftTop,
@@ -56,7 +56,7 @@
 	 * 	@value horizontal 水平显示
 	 * 	@value vertical 垂直显示
 	 * @property {Array} content 展开菜单内容配置项
-	 * @property {Boolean} onlyPlusButton 是否只显示加号按钮
+	 * @property {Boolean} popMenu 是否使用弹出菜单
 	 * @event {Function} trigger 展开菜单点击事件，返回点击信息
 	 */
 	export default {
@@ -90,9 +90,9 @@
 				type: Boolean,
 				default: false
 			},
-			onlyPlusButton: {
+			popMenu: {
 				type: Boolean,
-				default: false
+				default: true
 			}
 		},
 		data() {
@@ -168,7 +168,7 @@
 		methods: {
 			_onClick() {
 				this.$emit('plusClick')
-				if (this.onlyPlusButton) {
+				if (!this.popMenu) {
 					return
 				}
 				this.isShow = !this.isShow
