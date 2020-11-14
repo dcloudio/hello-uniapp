b79811f983f38de521b3732e0cc5c6b93689a2d3
tianjiaxing <tianjiaxing2016@163.com>
2020-11-14 11:14:53 +0800

    update

--- pages/component/navigator/navigator.vue
+++ pages/component/navigator/navigator.vue
@@ -10,7 +10,7 @@
 					<button type="default">在当前页打开</button>
 				</navigator>
 				<navigator v-if="!hasLeftWin" url="/pages/tabBar/extUI/extUI" open-type="switchTab" hover-class="other-navigator-hover">
-					<button type="default">跳转tab页面{{$hasLeftWin}}</button>
+					<button type="default">跳转tab页面</button>
 				</navigator>
 			</view>
 		</view>
