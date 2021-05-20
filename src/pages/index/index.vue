f3a51d62eec8d3c2a2dbd8c7cb64ae3dc0c2a83c
tianjiaxing <tianjiaxing2016@163.com>
2021-05-20 16:56:50 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+    }
+  },
+  onLoad() {},
+  mounted() {
+      const observer = uni.createIntersectionObserver(this);
+      console.log(111, observer)
+      const observer2 = uni.createMediaQueryObserver(this);
+      // console.log(222, observer2)
+  },
+  methods: {},
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
