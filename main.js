2d0e0d55fa3b95a2e96f7daecac881b74b3fb331
tianjiaxing <tianjiaxing2016@163.com>
2021-07-28 18:22:22 +0800

    update main.js

--- main.js
+++ main.js
@@ -4,27 +4,36 @@ import store from './store'
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
-
 Vue.prototype.$store = store
-Vue.prototype.$backgroundAudioData = {
-  playing: false,
-  playTime: 0,
-  formatedPlayTime: '00:00:00'
-}
 Vue.prototype.$adpid = "1111111111"
-
+Vue.prototype.$backgroundAudioData = {
+	playing: false,
+	playTime: 0,
+	formatedPlayTime: '00:00:00'
+}
 App.mpType = 'app'
-
 const app = new Vue({
-  store,
-  ...App
+	store,
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createApp } from 'vue'
-const app = createApp(App,{})
-app.use(store)
-app.mount('#app')
+import {
+	createSSRApp
+} from 'vue'
+export function createApp() {
+	const app = createSSRApp(App)
+	app.use(store)
+	app.config.globalProperties.$adpid = "1111111111"
+	app.config.globalProperties.$backgroundAudioData = {
+		playing: false,
+		playTime: 0,
+		formatedPlayTime: '00:00:00'
+	}
+	return {
+		app
+	}
+}
 // #endif
