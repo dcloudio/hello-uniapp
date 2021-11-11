9cedb43958cdbe33dcd724018c26057617049d97
tianjiaxing <tianjiaxing2016@163.com>
2021-11-11 15:50:04 +0800

    chore: update console version

--- App.vue
+++ App.vue
@@ -1,42 +1,44 @@
 <script>
-	import { mapMutations } from 'vuex'
+	import {
+		mapMutations
+	} from 'vuex'
 	import {
 		version
 	} from './package.json'
-    export default {
-        onLaunch: function() {
-            console.log('App Launch');
+	export default {
+		onLaunch: function() {
 			console.log(
-							`%c hello uniapp %c v${version} %c`,
-							'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
-							'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;',
-							'background:transparent'
-						)
-						console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
-            // #ifdef APP-PLUS
-            // App平台检测升级，服务端代码是通过uniCloud的云函数实现的，详情可参考：https://ext.dcloud.net.cn/plugin?id=2226
-			if(plus.runtime.appid !== 'HBuilder'){ // 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
+				`%c hello uniapp %c v${version} %c`,
+				'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+				'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;',
+				'background:transparent'
+			)
+			console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
+			console.log('App Launch');
+			// #ifdef APP-PLUS
+			// App平台检测升级，服务端代码是通过uniCloud的云函数实现的，详情可参考：https://ext.dcloud.net.cn/plugin?id=2226
+			if (plus.runtime.appid !== 'HBuilder') { // 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
 				uni.request({
-				    url: 'https://7a3e3fa9-7820-41d0-be80-11927ac2026c.bspapp.com/http/update', //检查更新的服务器地址
-				    data: {
-				        appid: plus.runtime.appid,
-				        version: plus.runtime.version,
-				        imei: plus.device.imei
-				    },
-				    success: (res) => {
-				        if (res.statusCode == 200 && res.data.isUpdate) {
-				            // 提醒用户更新
-				            uni.showModal({
-				                title: '更新提示',
-				                content: res.data.note ? res.data.note : '是否选择更新',
-				                success: (ee) => {
-				                    if (ee.confirm) {
-				                        plus.runtime.openURL(res.data.url);
-				                    }
-				                }
-				            })
-				        }
-				    }
+					url: 'https://7a3e3fa9-7820-41d0-be80-11927ac2026c.bspapp.com/http/update', //检查更新的服务器地址
+					data: {
+						appid: plus.runtime.appid,
+						version: plus.runtime.version,
+						imei: plus.device.imei
+					},
+					success: (res) => {
+						if (res.statusCode == 200 && res.data.isUpdate) {
+							// 提醒用户更新
+							uni.showModal({
+								title: '更新提示',
+								content: res.data.note ? res.data.note : '是否选择更新',
+								success: (ee) => {
+									if (ee.confirm) {
+										plus.runtime.openURL(res.data.url);
+									}
+								}
+							})
+						}
+					}
 				})
 			}
 
@@ -55,37 +57,37 @@
 					console.log("preLogin fail res: ", res);
 				}
 			})
-            // #endif
-        },
-        onShow: function() {
-            console.log('App Show')
-        },
-        onHide: function() {
-            console.log('App Hide')
-        },
+			// #endif
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
 		globalData: {
 			test: ''
 		},
-		methods:{
-			...mapMutations(['setUniverifyErrorMsg','setUniverifyLogin'])
+		methods: {
+			...mapMutations(['setUniverifyErrorMsg', 'setUniverifyLogin'])
 		}
-    }
+	}
 </script>
 
 <style>
-    /* #ifndef APP-PLUS-NVUE */
-    /* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
-    @import './common/uni.css';
+	/* #ifndef APP-PLUS-NVUE */
+	/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
+	@import './common/uni.css';
 
 	/* H5 兼容 pc 所需 */
 	/* #ifdef H5 */
 	@media screen and (min-width: 768px) {
-		body{
+		body {
 			overflow-y: scroll;
 		}
 	}
 
-	 /* 顶栏通栏样式 */
+	/* 顶栏通栏样式 */
 	/* .uni-top-window {
 	    left: 0;
 	    right: 0;
@@ -104,43 +106,47 @@
 	.uni-app--showleftwindow .hideOnPc {
 		display: none !important;
 	}
+
 	/* #endif */
 
-    /* 以下样式用于 hello uni-app 演示所需 */
-    page {
-        background-color: #efeff4;
-        height: 100%;
-        font-size: 28rpx;
-        line-height: 1.8;
-    }
+	/* 以下样式用于 hello uni-app 演示所需 */
+	page {
+		background-color: #efeff4;
+		height: 100%;
+		font-size: 28rpx;
+		line-height: 1.8;
+	}
+
 	.fix-pc-padding {
 		padding: 0 50px;
 	}
-    .uni-header-logo {
-        padding: 30rpx;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin-top: 10rpx;
-    }
 
-    .uni-header-image {
-        width: 100px;
-        height: 100px;
-    }
+	.uni-header-logo {
+		padding: 30rpx;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-top: 10rpx;
+	}
 
-    .uni-hello-text {
-        color: #7A7E83;
-    }
+	.uni-header-image {
+		width: 100px;
+		height: 100px;
+	}
 
-    .uni-hello-addfile {
-        text-align: center;
-        line-height: 300rpx;
-        background: #FFF;
-        padding: 50rpx;
-        margin-top: 10px;
-        font-size: 38rpx;
-        color: #808080;
-    }
-    /* #endif*/
+	.uni-hello-text {
+		color: #7A7E83;
+	}
+
+	.uni-hello-addfile {
+		text-align: center;
+		line-height: 300rpx;
+		background: #FFF;
+		padding: 50rpx;
+		margin-top: 10px;
+		font-size: 38rpx;
+		color: #808080;
+	}
+
+	/* #endif*/
 </style>
