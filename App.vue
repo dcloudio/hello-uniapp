d45829f98085c852d187f4ed05c14061480c970d
tianjiaxing <tianjiaxing2016@163.com>
2021-11-11 15:29:00 +0800

    chore: console job invite ad

--- App.vue
+++ App.vue
@@ -1,8 +1,18 @@
 <script>
 	import { mapMutations } from 'vuex'
+	import {
+		version
+	} from './package.json'
     export default {
         onLaunch: function() {
             console.log('App Launch');
+			console.log(
+							`%c hello uniapp %c v${version} %c`,
+							'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+							'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;',
+							'background:transparent'
+						)
+						console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
             // #ifdef APP-PLUS
             // App平台检测升级，服务端代码是通过uniCloud的云函数实现的，详情可参考：https://ext.dcloud.net.cn/plugin?id=2226
 			if(plus.runtime.appid !== 'HBuilder'){ // 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
