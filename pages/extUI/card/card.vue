19324b65f52f98a1aca38457e395f75a5bc5ecf0
mehaotian <490272692@qq.com>
2022-03-16 18:13:32 +0800

    update uni-card 示例

--- pages/extUI/card/card.vue
+++ pages/extUI/card/card.vue
@@ -3,82 +3,77 @@
 		<uni-card :is-shadow="false" is-full>
 			<text class="uni-h6">卡片组件通用来显示完整独立的一段信息，同时让用户理解他的作用。例如一篇文章的预览图、作者信息、时间等，卡片通常是更复杂和更详细信息的入口点。</text>
 		</uni-card>
-		<uni-section title="基础卡片" type="line">
+		<uni-section overflow title="基础卡片" type="line">
 			<uni-card :is-shadow="false">
 				<text class="uni-body">这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
 			</uni-card>
 		</uni-section>
-		<uni-section title="卡片标题+额外信息" type="line">
+		<uni-section overflow title="卡片标题+额外信息" type="line">
 			<uni-card title="基础卡片" extra="额外信息">
 				<text class="uni-body">这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
 			</uni-card>
 		</uni-section>
 
-		<uni-section title="双标题卡片" type="line">
+		<uni-section overflow title="双标题卡片" type="line" >
 			<uni-card title="基础卡片" sub-title="副标题" extra="额外信息" :thumbnail="avatar" @click="onClick">
 				<text class="uni-body">这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
 			</uni-card>
 		</uni-section>
 
-		<uni-section title="通栏卡片" type="line">
+		<uni-section overflow title="通栏卡片" type="line">
 			<uni-card title="基础卡片" :isFull="true" sub-title="副标题" extra="额外信息" :thumbnail="avatar">
 				<text class="uni-body">这是一个通栏卡片 ，通栏没有外边距，左右会贴合父元素。</text>
 			</uni-card>
 		</uni-section>
 
-		<uni-section title="卡片封面图+操作栏" type="line">
+		<uni-section overflow title="卡片封面图+操作栏" type="line">
 			<uni-card :cover="cover" @click="onClick">
 				<!-- <image slot='cover' style="width: 100%;" :src="cover"></image> -->
 				<text class="uni-body">这是一个带封面和操作栏的卡片示例，此示例展示了封面插槽和操作栏插槽的用法。</text>
-				<template v-slot:actions>
-					<view class="card-actions no-border">
-						<view class="card-actions-item" @click="actionsClick('分享')">
-							<uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">分享</text>
-						</view>
-						<view class="card-actions-item" @click="actionsClick('点赞')">
-							<uni-icons type="heart" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">点赞</text>
-						</view>
-						<view class="card-actions-item" @click="actionsClick('评论')">
-							<uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">评论</text>
-						</view>
+				<view slot="actions" class="card-actions">
+					<view class="card-actions-item" @click="actionsClick('分享')">
+						<uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">分享</text>
 					</view>
-				</template>
+					<view class="card-actions-item" @click="actionsClick('点赞')">
+						<uni-icons type="heart" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">点赞</text>
+					</view>
+					<view class="card-actions-item" @click="actionsClick('评论')">
+						<uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">评论</text>
+					</view>
+				</view>
 			</uni-card>
 		</uni-section>
 
-		<uni-section title="自定义卡片内容" type="line">
-			<uni-card title="基础卡片" sub-title="副标题" extra="额外信息" padding="10px 0" :thumbnail="avatar">
+		<uni-section overflow title="自定义卡片内容" type="line">
+			<uni-card title="基础卡片" sub-title="副标题" extra="额外信息" padding="10px 0" :thumbnail="avatar" >
 				<template v-slot:title>
 					<uni-list>
-						<uni-list-item :show-switch="true" title="自定义标题" @switchChange="switchChange" />
+						<uni-list-item :show-switch="true" title="自定义标题"/>
 					</uni-list>
 				</template>
 				<image style="width: 100%;" :src="cover"></image>
-				<text
-					class="uni-body uni-mt-5">卡片组件通用来显示完整独立的一段信息，同时让用户理解他的作用。例如一篇文章的预览图、作者信息、时间等，卡片通常是更复杂和更详细信息的入口点。</text>
-				<template v-slot:actions>
-					<view class="card-actions no-border">
-						<view class="card-actions-item" @click="actionsClick('分享')">
-							<uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">分享</text>
-						</view>
-						<view class="card-actions-item" @click="actionsClick('点赞')">
-							<uni-icons type="heart" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">点赞</text>
-						</view>
-						<view class="card-actions-item" @click="actionsClick('评论')">
-							<uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">评论</text>
-						</view>
+				<text class="uni-body uni-mt-5">卡片组件通用来显示完整独立的一段信息，同时让用户理解他的作用。例如一篇文章的预览图、作者信息、时间等，卡片通常是更复杂和更详细信息的入口点。</text>
+				<view slot="actions" class="card-actions">
+					<view class="card-actions-item" @click="actionsClick('分享')">
+						<uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">分享</text>
 					</view>
-				</template>
+					<view class="card-actions-item" @click="actionsClick('点赞')">
+						<uni-icons type="heart" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">点赞</text>
+					</view>
+					<view class="card-actions-item" @click="actionsClick('评论')">
+						<uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">评论</text>
+					</view>
+				</view>
 			</uni-card>
 		</uni-section>
 
-		<uni-section title="卡片+列表" type="line">
+		<uni-section overflow title="卡片+列表" type="line">
 			<uni-card padding="0" spacing="0">
 				<template v-slot:cover>
 					<view class="custom-cover">
@@ -93,22 +88,20 @@
 					<uni-list-item title="今日新闻" showArrow></uni-list-item>
 					<uni-list-item title="今日新闻" showArrow></uni-list-item>
 				</uni-list>
-				<template v-slot:actions>
-					<view class="card-actions no-border">
-						<view class="card-actions-item" @click="actionsClick('分享')">
-							<uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">分享</text>
-						</view>
-						<view class="card-actions-item" @click="actionsClick('点赞')">
-							<uni-icons type="heart" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">点赞</text>
-						</view>
-						<view class="card-actions-item" @click="actionsClick('评论')">
-							<uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
-							<text class="card-actions-item-text">评论</text>
-						</view>
+				<view slot="actions" class="card-actions no-border">
+					<view class="card-actions-item" @click="actionsClick('分享')">
+						<uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">分享</text>
 					</view>
-				</template>
+					<view class="card-actions-item" @click="actionsClick('点赞')">
+						<uni-icons type="heart" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">点赞</text>
+					</view>
+					<view class="card-actions-item" @click="actionsClick('评论')">
+						<uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
+						<text class="card-actions-item-text">评论</text>
+					</view>
+				</view>
 			</uni-card>
 		</uni-section>
 
@@ -123,7 +116,7 @@
 			return {
 				cover: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/094a9dc0-50c0-11eb-b680-7980c8a877b8.jpg',
 				avatar: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png',
-				extraIcon: {
+				extraIcon:{
 					color: '#4cd964',
 					size: '22',
 					type: 'gear-filled'
@@ -131,13 +124,13 @@
 			}
 		},
 		methods: {
-			onClick(e) {
+			onClick(e){
 				console.log(e)
 			},
-			actionsClick(text) {
+			actionsClick(text){
 				uni.showToast({
-					title: text,
-					icon: 'none'
+					title:text,
+					icon:'none'
 				})
 			}
 		}
@@ -145,6 +138,7 @@
 </script>
 
 <style lang="scss">
+
 	.container {
 		overflow: hidden;
 	}
@@ -178,24 +172,20 @@
 		height: 45px;
 		border-top: 1px #eee solid;
 	}
-
 	.card-actions-item {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 	}
-
 	.card-actions-item-text {
 		font-size: 12px;
 		color: #666;
 		margin-left: 5px;
 	}
-
 	.cover-image {
 		flex: 1;
 		height: 150px;
 	}
-
 	.no-border {
 		border-width: 0;
 	}
