f06825f3449ddb7dbbe461fc7d31d6cf904ab3f9
anne-lxm <1076217653@qq.com>
2022-08-10 18:24:29 +0800

    更新editor示例支持百度小程序

--- pages/component/editor/editor.vue
+++ pages/component/editor/editor.vue
@@ -7,65 +7,55 @@
 					<view :class="formats.italic ? 'ql-active' : ''" class="iconfont icon-zitixieti" data-name="italic"></view>
 					<view :class="formats.underline ? 'ql-active' : ''" class="iconfont icon-zitixiahuaxian" data-name="underline"></view>
 					<view :class="formats.strike ? 'ql-active' : ''" class="iconfont icon-zitishanchuxian" data-name="strike"></view>
-					<view :class="formats.align === 'left' ? 'ql-active' : ''" class="iconfont icon-zuoduiqi" data-name="align"
-					 data-value="left"></view>
-					<view :class="formats.align === 'center' ? 'ql-active' : ''" class="iconfont icon-juzhongduiqi" data-name="align"
-					 data-value="center"></view>
-					<view :class="formats.align === 'right' ? 'ql-active' : ''" class="iconfont icon-youduiqi" data-name="align"
-					 data-value="right"></view>
-					<view :class="formats.align === 'justify' ? 'ql-active' : ''" class="iconfont icon-zuoyouduiqi" data-name="align"
-					 data-value="justify"></view>
-					<view :class="formats.lineHeight ? 'ql-active' : ''" class="iconfont icon-line-height" data-name="lineHeight"
-					 data-value="2"></view>
-					<view :class="formats.letterSpacing ? 'ql-active' : ''" class="iconfont icon-Character-Spacing" data-name="letterSpacing"
-					 data-value="2em"></view>
-					<view :class="formats.marginTop ? 'ql-active' : ''" class="iconfont icon-722bianjiqi_duanqianju" data-name="marginTop"
-					 data-value="20px"></view>
-					<view :class="formats.previewarginBottom ? 'ql-active' : ''" class="iconfont icon-723bianjiqi_duanhouju" data-name="marginBottom"
-					 data-value="20px"></view>
+					<!-- #ifndef MP-BAIDU -->
+					<view :class="formats.align === 'left' ? 'ql-active' : ''" class="iconfont icon-zuoduiqi" data-name="align" data-value="left"></view>
+					<!-- #endif -->
+					<view :class="formats.align === 'center' ? 'ql-active' : ''" class="iconfont icon-juzhongduiqi" data-name="align" data-value="center"></view>
+					<view :class="formats.align === 'right' ? 'ql-active' : ''" class="iconfont icon-youduiqi" data-name="align" data-value="right"></view>
+					<view :class="formats.align === 'justify' ? 'ql-active' : ''" class="iconfont icon-zuoyouduiqi" data-name="align" data-value="justify"></view>
+					<!-- #ifndef MP-BAIDU -->
+					<view :class="formats.lineHeight ? 'ql-active' : ''" class="iconfont icon-line-height" data-name="lineHeight" data-value="2"></view>
+					<view :class="formats.letterSpacing ? 'ql-active' : ''" class="iconfont icon-Character-Spacing" data-name="letterSpacing" data-value="2em"></view>
+					<view :class="formats.marginTop ? 'ql-active' : ''" class="iconfont icon-722bianjiqi_duanqianju" data-name="marginTop" data-value="20px"></view>
+					<view :class="formats.marginBottom ? 'ql-active' : ''" class="iconfont icon-723bianjiqi_duanhouju" data-name="marginBottom" data-value="20px"></view>
+					<!-- #endif -->
+					
 					<view class="iconfont icon-clearedformat" @tap="removeFormat"></view>
+					
+					<!-- #ifndef MP-BAIDU -->
 					<view :class="formats.fontFamily ? 'ql-active' : ''" class="iconfont icon-font" data-name="fontFamily" data-value="Pacifico"></view>
-					<view :class="formats.fontSize === '24px' ? 'ql-active' : ''" class="iconfont icon-fontsize" data-name="fontSize"
-					 data-value="24px"></view>
-
-					<view :class="formats.color === '#0000ff' ? 'ql-active' : ''" class="iconfont icon-text_color" data-name="color"
-					 data-value="#0000ff"></view>
-					<view :class="formats.backgroundColor === '#00ff00' ? 'ql-active' : ''" class="iconfont icon-fontbgcolor"
-					 data-name="backgroundColor" data-value="#00ff00"></view>
-
+					<view :class="formats.fontSize === '24px' ? 'ql-active' : ''" class="iconfont icon-fontsize" data-name="fontSize" data-value="24px"></view>
+					<!-- #endif -->
+					<view :class="formats.color === '#0000ff' ? 'ql-active' : ''" class="iconfont icon-text_color" data-name="color" data-value="#0000ff"></view>
+					<view :class="formats.backgroundColor === '#00ff00' ? 'ql-active' : ''" class="iconfont icon-fontbgcolor" data-name="backgroundColor" data-value="#00ff00"></view>
 					<view class="iconfont icon-date" @tap="insertDate"></view>
 					<view class="iconfont icon--checklist" data-name="list" data-value="check"></view>
-					<view :class="formats.list === 'ordered' ? 'ql-active' : ''" class="iconfont icon-youxupailie" data-name="list"
-					 data-value="ordered"></view>
-					<view :class="formats.list === 'bullet' ? 'ql-active' : ''" class="iconfont icon-wuxupailie" data-name="list"
-					 data-value="bullet"></view>
+					<view :class="formats.list === 'ordered' ? 'ql-active' : ''" class="iconfont icon-youxupailie" data-name="list" data-value="ordered"></view>
+					<view :class="formats.list === 'bullet' ? 'ql-active' : ''" class="iconfont icon-wuxupailie" data-name="list" data-value="bullet"></view>
+					
 					<view class="iconfont icon-undo" @tap="undo"></view>
 					<view class="iconfont icon-redo" @tap="redo"></view>
-
+					
 					<view class="iconfont icon-outdent" data-name="indent" data-value="-1"></view>
 					<view class="iconfont icon-indent" data-name="indent" data-value="+1"></view>
 					<view class="iconfont icon-fengexian" @tap="insertDivider"></view>
 					<view class="iconfont icon-charutupian" @tap="insertImage"></view>
-					<view :class="formats.header === 1 ? 'ql-active' : ''" class="iconfont icon-format-header-1" data-name="header"
-					 :data-value="1"></view>
-					<view :class="formats.script === 'sub' ? 'ql-active' : ''" class="iconfont icon-zitixiabiao" data-name="script"
-					 data-value="sub"></view>
-					<view :class="formats.script === 'super' ? 'ql-active' : ''" class="iconfont icon-zitishangbiao" data-name="script"
-					 data-value="super"></view>
+					<view :class="formats.header === 1 ? 'ql-active' : ''" class="iconfont icon-format-header-1" data-name="header" :data-value="1"></view>
+					<view :class="formats.script === 'sub' ? 'ql-active' : ''" class="iconfont icon-zitixiabiao" data-name="script" data-value="sub"></view>
+					<view :class="formats.script === 'super' ? 'ql-active' : ''" class="iconfont icon-zitishangbiao" data-name="script" data-value="super"></view>
+					
 					<view class="iconfont icon-shanchu" @tap="clear"></view>
-					<view :class="formats.direction === 'rtl' ? 'ql-active' : ''" class="iconfont icon-direction-rtl" data-name="direction"
-					 data-value="rtl"></view>
-
+					
+					<view :class="formats.direction === 'rtl' ? 'ql-active' : ''" class="iconfont icon-direction-rtl" data-name="direction" data-value="rtl"></view>
 				</view>
-
+				
 				<view class="editor-wrapper">
-					<editor id="editor" class="ql-container" placeholder="开始输入..." showImgSize showImgToolbar showImgResize
-					 @statuschange="onStatusChange" :read-only="readOnly" @ready="onEditorReady">
+					<editor id="editor" class="ql-container" placeholder="开始输入..." show-img-size show-img-toolbar
+						show-img-resize @statuschange="onStatusChange" :read-only="readOnly" @ready="onEditorReady">
 					</editor>
 				</view>
 			</view>
 		</view>
-
 	</view>
 </template>
 
@@ -77,14 +67,28 @@
 				formats: {}
 			}
 		},
+		onLoad() {
+			// #ifndef MP-BAIDU 
+			uni.loadFontFace({
+				family: 'Pacifico',
+				source: 'url("https://sungd.github.io/Pacifico.ttf")'
+			})
+			// #endif
+		},
 		methods: {
 			readOnlyChange() {
 				this.readOnly = !this.readOnly
 			},
 			onEditorReady() {
+				// #ifdef MP-BAIDU
+				this.editorCtx = requireDynamicLib('editorLib').createEditorContext('editor');
+				// #endif
+
+				// #ifdef APP-PLUS || MP-WEIXIN || H5
 				uni.createSelectorQuery().select('#editor').context((res) => {
 					this.editorCtx = res.context
 				}).exec()
+				// #endif
 			},
 			undo() {
 				this.editorCtx.undo()
@@ -100,7 +104,6 @@
 				if (!name) return
 				// console.log('format', name, value)
 				this.editorCtx.format(name, value)
-
 			},
 			onStatusChange(e) {
 				const formats = e.detail
@@ -114,9 +117,17 @@
 				})
 			},
 			clear() {
-				this.editorCtx.clear({
-					success: function(res) {
-						console.log("clear success")
+				uni.showModal({
+					title: '清空编辑器',
+					content: '确定清空编辑器全部内容？',
+					success: res => {
+						if (res.confirm) {
+							this.editorCtx.clear({
+								success: function(res) {
+									console.log("clear success")
+								}
+							})
+						}
 					}
 				})
 			},
@@ -144,13 +155,7 @@
 					}
 				})
 			}
-		},
-		onLoad() {
-			uni.loadFontFace({
-				family: 'Pacifico',
-				source: 'url("https://sungd.github.io/Pacifico.ttf")'
-			})
-		},
+		}
 	}
 </script>
 
@@ -184,7 +189,6 @@
 		border-bottom: 0;
 		font-family: 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
 	}
-
 
 	.ql-container {
 		box-sizing: border-box;
