03fc71ca23f23ac9c3595bb3022792628a135b32
zhenyuWang <13641039885@163.com>
2023-10-11 17:44:24 +0800

    test: 添加 jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testTimeout: 20000,
+    reporters: [
+        'default'
+    ],
+    watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+    moduleFileExtensions: ['js', 'json'],
+    rootDir: __dirname,
+    testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
+    testPathIgnorePatterns: ['/node_modules/']
+}
