f55407e774c093a83d2747f13713faef12fbbddf
Anne_LXM <liuxiaomei@dcloud.io>
2024-08-16 19:48:27 +0800

    test：优化测试例

--- pages/API/download-file/download-file.test.js
+++ pages/API/download-file/download-file.test.js
@@ -1,5 +1,4 @@
 // uni-app自动化测试教程: uni-app自动化测试教程: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/
-
 describe('pages/API/download-file/download-file.vue', () => {
 
 	let page;
@@ -9,11 +8,14 @@ describe('pages/API/download-file/download-file.vue', () => {
 	});
 
 	it('check download url', async () => {
-		expect.assertions(1);
+		expect.assertions(2);
 		await page.callMethod('downloadImage')
-		await page.waitFor(2000)
-		const data = await page.data()
-		console.log("data",data)
+		const start = Date.now();
+		await page.waitFor(async () => {
+		  return await page.data('jest_result') === true || (Date.now() - start > 2000)
+		})
+		console.log("data",await page.data(),Date.now() - start > 2000)
 		expect(await page.data('jest_result')).toBeTruthy();
+		expect(await page.data('imageSrc')).toBeTruthy();
 	});
 });
--- pages/tabBar/extUI/extUI.test.js
+++ pages/tabBar/extUI/extUI.test.js
@@ -9,9 +9,8 @@ describe('pages/tabBar/extUI/extUI.nvue', () => {
         if (process.env.UNI_PLATFORM === "mp-weixin" || process.env.UNI_PLATFORM === "mp-baidu") {
         	await page.waitFor(10000)
         }
-        
         page = await program.currentPage()
-
+		await page.waitFor("view")
     })
     /**
      * 点击 u-link 组件
@@ -19,36 +18,32 @@ describe('pages/tabBar/extUI/extUI.nvue', () => {
     it('u-link 点击', async () => {
         const helloLink = await page.$('.hello-link')
         // 检测首页u-link的文本内容
-        expect(await helloLink.text()).toBe(
-            'https://ext.dcloud.net.cn')
+        expect(await helloLink.text()).toBe('https://ext.dcloud.net.cn')
     })
 
     /**
      * 检测列表长度,是否发生变化
      */
-    // it('.uni-panel 列表长度', async () => {
-    //     const lists = await page.$$('.uni-panel')
-    //     expect(lists.length).toBe(29)
-    // })
-
+    it('.uni-panel 列表长度', async () => {
+		const lists = await page.$$('.uni-panel')
+		if(process.env.UNI_PLATFORM == 'app-plus'){
+			expect(lists.length).toBe(41)
+		}else if(process.env.UNI_PLATFORM == 'h5' || process.env.UNI_PLATFORM == 'mp-weixin'){
+			expect(lists.length).toBe(44)
+		}
+    })
     /**
      * 检测打开列表第一个元素是否正确
      */
     it('.uni-panel-h tap 打开 badge 组件', async () => {
         // 检测首个 panel 是视图容器
-        expect(await (await page.$('.uni-panel-text')).text()).toBe(
-            'uni-badge 数字角标')
-
+        expect(await (await page.$('.uni-panel-text')).text()).toBe('uni-badge 数字角标')
         const listHead = await page.$('.uni-panel-h')
         // 是否获取正确的元素
-        expect(await listHead.attribute('class')).toContain(
-            'uni-panel-h',
-        )
-
+        expect(await listHead.attribute('class')).toContain('uni-panel-h')
         // 点击第一个 item，验证打开的新页面是否正确
         await listHead.tap()
-        await page.waitFor(2000)
-        
+		await page.waitFor(2000)
         expect((await program.currentPage()).path).toBe('pages/extUI/badge/badge')
         await page.waitFor(500)
         // 执行 navigateBack 验证是否返回
