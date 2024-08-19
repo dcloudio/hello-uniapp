6c80ce0dad2b4e7ffe4ebae0f74c74fb67bf240c
Anne_LXM <liuxiaomei@dcloud.io>
2024-08-19 20:25:10 +0800

    test(download-file.test.js)：调整测试例

--- pages/API/download-file/download-file.test.js
+++ pages/API/download-file/download-file.test.js
@@ -4,17 +4,18 @@ describe('pages/API/download-file/download-file.vue', () => {
 	let page;
 	beforeAll(async () => {
 		page = await program.reLaunch('/pages/API/download-file/download-file')
-		await page.waitFor(1000)
+		await page.waitFor('view')
 	});
 
 	it('check download url', async () => {
 		expect.assertions(2);
 		await page.callMethod('downloadImage')
+		const waitTime = process.env.uniTestPlatformInfo.includes('firefox') ? 5000:2000
 		const start = Date.now();
 		await page.waitFor(async () => {
-		  return await page.data('jest_result') === true || (Date.now() - start > 2000)
+		  return await page.data('jest_result') === true || (Date.now() - start > waitTime)
 		})
-		console.log("data",await page.data(),Date.now() - start > 2000)
+		console.log("data",await page.data(),Date.now() - start > waitTime)
 		expect(await page.data('jest_result')).toBeTruthy();
 		expect(await page.data('imageSrc')).toBeTruthy();
 	});
