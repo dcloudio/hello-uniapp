6a8d6c1a2410050537b8121bd886a5ac2ac25558
anne-xml <liuxiaomei@dcloud.io>
2024-11-20 14:45:47 +0800

    新增renderjs测试例

---
+++ pages/template/renderjs/renderjs.test.js
@@ -0,0 +1,28 @@
+describe('renderjs', () => {
+  let page,btns;
+  beforeAll(async () => {
+    page = await program.reLaunch('/pages/template/renderjs/renderjs')
+    await page.waitFor('view');
+    btns = await page.$$('button');
+  });
+  it('check page data', async () => {
+    const data1 = await page.$('.renderjs');
+    expect(await data1.text()).toEqual('serviceData');
+  });
+  it('callMethod renderjs', async () => {
+    await btns[0].tap();
+    await page.waitFor(300);
+    const data2 = await page.$('.renderjs');
+    expect(await data2.text()).toEqual('renderjsData');
+  });
+  it('callMethod service', async () => {
+    await btns[1].tap();
+    await page.waitFor(300);
+    const data3 = await page.$('.renderjs');
+    expect(await data3.text()).toEqual('serviceData改变了');
+  });
+  it('trigger getServiceData', async () => {
+    expect(await page.data('newValue')).toEqual('serviceData改变了');
+    expect(await page.data('oldValue')).toEqual('renderjsData');
+  });
+});
