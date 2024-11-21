b4589f65cba0e7a015e9f6e1b4ef02723d1de29f
anne-xml <liuxiaomei@dcloud.io>
2024-11-21 19:28:02 +0800

    更新renderjs测试例兼容app

--- pages/template/renderjs/renderjs.test.js
+++ pages/template/renderjs/renderjs.test.js
@@ -1,37 +1,38 @@
 describe('renderjs', () => {
-  let page,btns;
-  if (process.env.UNI_PLATFORM === "mp-weixin") {
-  	// renderjs仅支持App和H5
-    it('is mp-weixin', async () => {
-      expect(1).toBe(1)
-    })
-    return
-  }
-  beforeAll(async () => {
-    page = await program.reLaunch('/pages/template/renderjs/renderjs')
-    await page.waitFor('view');
-    btns = await page.$$('button');
-  });
-  it('check page data', async () => {
-    const data1 = await page.$('.renderjs');
-    expect(await data1.text()).toEqual('serviceData');
-  });
-  it('callMethod renderjs', async () => {
-    await btns[0].tap();
-    await page.waitFor(300);
-    const data2 = await page.$('.renderjs');
-    expect(await data2.text()).toEqual('renderjsData');
-  });
-  it('callMethod service', async () => {
-    await btns[1].tap();
-    await page.waitFor(300);
-    const data3 = await page.$('.renderjs');
-    expect(await data3.text()).toEqual('serviceData改变了');
-  });
-  it('trigger getServiceData', async () => {
-	await page.waitFor(300);
-	console.log('data: ',await page.data());
-    expect(await page.data('newValue')).toEqual('serviceData改变了');
-    expect(await page.data('oldValue')).toEqual('renderjsData');
-  });
+	let page, btns;
+	if (process.env.UNI_PLATFORM === "mp-weixin") {
+		// renderjs仅支持App和H5
+		it('is mp-weixin', async () => {
+			expect(1).toBe(1)
+		})
+		return
+	}
+	beforeAll(async () => {
+		page = await program.reLaunch('/pages/template/renderjs/renderjs')
+		await page.waitFor('view');
+		btns = await page.$$('button');
+	});
+	it('check page data', async () => {
+		const data1 = await page.$('.renderjs');
+		expect(await data1.text()).toEqual('serviceData');
+	});
+	it('callMethod renderjs', async () => {
+		await btns[0].tap();
+		await page.waitFor(300);
+		const data2 = await page.$('.renderjs');
+		expect(await data2.text()).toEqual('renderjsData');
+	});
+	it('callMethod service', async () => {
+		await btns[1].tap();
+		await page.waitFor(300);
+		const data3 = await page.$('.renderjs');
+		expect(await data3.text()).toEqual('serviceData改变了');
+	});
+	if (process.env.UNI_PLATFORM === "h5") {
+		it('trigger getServiceData', async () => {
+			expect(await page.data('newValue')).toEqual('serviceData改变了');
+			expect(await page.data('oldValue')).toEqual('renderjsData');
+		});
+	}
+
 });
