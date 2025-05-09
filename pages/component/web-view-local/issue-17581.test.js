f21b1176bff8f6e1ea3726ce6385b4568cea97c7
王亚琪 <wangyaqi988@163.com>
2025-05-09 14:42:57 +0800

    test: 增加web-view点击位置偏移测试例

---
+++ pages/component/web-view-local/issue-17581.test.js
@@ -0,0 +1,29 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isHarmony = platformInfo.startsWith('harmony')
+
+describe('pages/component/web-view-local/issue-17581.vue', () => {
+	if (!isHarmony) {
+		it('skip', async () => {
+			expect(1).toBe(1)
+		})
+		return
+	}
+	let page
+	beforeAll(async () => {
+		page = await program.reLaunch('/pages/component/web-view-local/issue-17581')
+		await page.waitFor(1000)
+	})
+
+	it('click position', async () => {
+		await program.tap({
+			x: 100,
+			y: 400
+		})
+		await page.waitFor(200)
+		const {
+			clickClientY
+		} = await page.data()
+		expect(clickClientY).toBeLessThan(200)
+		expect(clickClientY).toBeGreaterThan(0)
+	})
+})
---
+++ pages/component/web-view-local/issue-17581.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<web-view style="margin-top: 200px;" src="/hybrid/html/issue-17581.html" @message="onWebViewMessage"></web-view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				clickClientX: 0,
+				clickClientY: 0
+			}
+		},
+		onLoad() {},
+		methods: {
+			onWebViewMessage(message) {
+				const data = message.detail.data
+				const dataItem = data && data[0]
+				if(dataItem.action === 'click') {
+					this.clickClientX = dataItem.clientX
+					this.clickClientY = dataItem.clientY
+				}
+			}
+		}
+	}
+</script>
+<style>
+</style>
