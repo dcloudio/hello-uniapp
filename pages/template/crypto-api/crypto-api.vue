984274059b28083c02b94e2265326bcc2ad9260c
jixinbao <jixinbao@dcloud.io>
2025-06-05 21:19:18 +0800

    feat: 补充 crypto 测试
    (cherry picked from commit 16a724fd12f3e3ffdc377cb4c4d4d8c581ede791)

---
+++ pages/template/crypto-api/crypto-api.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<view id="str">{{str.join('-')}}</view>
+		<button id="button" @click="getRandomValues">crypto.getRandomValues</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				str: [false, false, false]
+			}
+		},
+		methods: {
+			getRandomValues() {
+				try {
+					this.str[0] = typeof crypto === 'object';
+					this.str[1] = typeof crypto.getRandomValues === 'function';
+					var rnds8 = new Uint8Array(16);
+					const res = crypto.getRandomValues(rnds8);
+
+					this.str[2] = res.length === 16
+				} catch (e) {
+					console.log("crypto error:", e);
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
