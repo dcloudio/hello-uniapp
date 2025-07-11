1f8c40d689b9471b481a5cf43e2a7184e1a5fc22
jixinbao <jixinbao@dcloud.io>
2025-07-11 15:05:12 +0800

    feat: 补充动态图片下载

--- pages/API/download-file/download-file.test.js
+++ pages/API/download-file/download-file.test.js
@@ -1,13 +1,14 @@
 const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
 describe('pages/API/download-file/download-file.vue', () => {
+	/** @type {UniAutomatorPage} */
 	let page;
 	const versions = ['12.4', '13.7', '15.5', '17.4', '17.5_深色主题', '18.1'];
 	const [platform, version] = platformInfo.split(' ');
 	if (platform === 'ios_simulator' && versions.includes(version)) {
-	    it('skip', async () => {
-	        expect(1).toBe(1);
-	    });
-	    return;
+		it('skip', async () => {
+			expect(1).toBe(1);
+		});
+		return;
 	}
 	beforeAll(async () => {
 		page = await program.reLaunch('/pages/API/download-file/download-file')
@@ -19,9 +20,22 @@ describe('pages/API/download-file/download-file.vue', () => {
 		const waitTime = process.env.uniTestPlatformInfo.includes('firefox') ? 5000:2000
 		const start = Date.now();
 		await page.waitFor(async () => {
-		  return await page.data('jest_result') === true || (Date.now() - start > waitTime)
+			return await page.data('jest_result') === true || (Date.now() - start > waitTime)
 		})
 		expect(await page.data('jest_result')).toBeTruthy();
 		expect(await page.data('imageSrc')).toBeTruthy();
+	});
+	// 下载动态 url
+	it('下载动态 url 图片', async () => {
+		await page.setData({
+			imageSrc: 'https://open.weixin.qq.com/qr/code?username=dcloudio',
+		});
+		await page.callMethod('downloadImage');
+		await page.waitFor(2000);
+		expect(await page.data('jest_result')).toBeTruthy();
+		expect(await page.data('imageSrc')).toBeTruthy();
+		await program.screenshot({
+			fullPage: true,
+		});
 	});
 });
