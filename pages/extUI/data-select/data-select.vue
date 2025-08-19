f93d483b072b2c1b9020361ee2896d21b4045fc1
愈合 <1641284022@qq.com>
2025-08-19 16:25:16 +0800

    feat: 更新 data-select 示例和组件

--- pages/extUI/data-select/data-select.vue
+++ pages/extUI/data-select/data-select.vue
@@ -3,9 +3,15 @@
 		<uni-card :is-shadow="false" is-full>
 			<text class="uni-h6">通过数据驱动的单选框和复选框，可直接通过连接 uniCloud 获取数据，同时可以配合表单组件 uni-forms 使用</text>
 		</uni-card>
-		<uni-section title="本地数据" type="line">
+		<uni-section title="本地数据 (单选)" type="line">
 			<view class="uni-px-5 uni-pb-5">
 				<uni-data-select v-model="value" :localdata="range" @change="change"></uni-data-select>
+			</view>
+		</uni-section>
+		<uni-section title="多选换行显示" type="line">
+			<view class="uni-px-5 uni-pb-5">
+				<uni-data-select v-model="multipleValue" multiple wrap :localdata="range" @change="changeMultiple"
+					label="换行显示"></uni-data-select>
 			</view>
 		</uni-section>
 		<uni-section title="禁用状态" type="line">
@@ -13,52 +19,126 @@
 				<uni-data-select v-model="value" :disabled="true" :localdata="range"></uni-data-select>
 			</view>
 		</uni-section>
-		<!-- 		<uni-section title="云端数据" subTitle="连接云服务空间, 且存在相关的数据表才能生效(此处演示, 未连接云服务空间, 故不生效, 且有报错)" type="line">
+		<!-- <uni-section title="云端数据" subTitle="连接云服务空间, 且存在相关的数据表才能生效(此处演示, 未连接云服务空间, 故不生效, 且有报错)" type="line">
 			<uni-data-select collection="opendb-app-list" field="appid as value, name as text" v-mode="value" />
 		</uni-section> -->
-		<uni-section title="是否可清除已选项" type="line">
+		<uni-section title="设置弹出位置,文字出现位置,下划线显示" type="line">
 			<view class="uni-px-5 uni-pb-5">
-				<uni-data-select v-model="value" :localdata="range" @change="change" :clear="false"></uni-data-select>
+				<uni-data-select label="应用选择" placement="top" align="center" mode="underline" v-model="value" :localdata="range"
+					@change="change"></uni-data-select>
 			</view>
 		</uni-section>
-		<uni-section title="配置左侧标题" type="line">
+		<!-- #ifndef MP-TOUTIAO && VUE2 -->
+		<!-- 头条中存在 2 个插槽存在问题 -->
+		<uni-section title="单选使用插槽" type="line">
 			<view class="uni-px-5 uni-pb-5">
-				<uni-data-select v-model="value" :localdata="range" @change="change" label="应用选择"></uni-data-select>
+				<uni-data-select v-model="value" mode="underline" :localdata="range">
+					<template v-slot:selected="{selectedItems}">
+						<view class="slot-box">
+							<view v-for="item in sliceArray(selectedItems)" :key="item.value" class="slot-content-item">
+								{{item.text}}
+							</view>
+							<view v-if="selectedItems.length == 0" class="slot-content-item"> 没有选择项 </view>
+						</view>
+					</template>
+					<template v-slot:option="{item,itemSelected}">
+						<view class="item-view" :class="{
+							'item-selected': itemSelected,
+							'item-disabled': item.disable
+						}">
+							{{ item.text }}
+						</view>
+					</template>
+				</uni-data-select>
+			</view>
+		</uni-section>
+		<!-- #endif -->
+
+		<uni-section title="多选使用插槽" subTitle="多选的时候,超过2个显示省略;" type="line">
+			<view class="uni-px-5 uni-pb-5">
+				<uni-data-select v-model="multipleValue" multiple mode="underline" placement="top" :localdata="range">
+					<template v-slot:selected="{selectedItems}">
+						<view class="slot-box">
+							<view v-for="item in sliceArray(selectedItems)" :key="item.value" class="slot-content-item">
+								{{item.text}}
+							</view>
+							<view v-if="selectedItems.length > 2" class="slot-content-item"> +{{selectedItems.length - 2}} more </view>
+							<view v-if="selectedItems.length == 0" class="slot-content-item"> 没有选择项 </view>
+						</view>
+					</template>
+					<template v-slot:option="{item,itemSelected}">
+						<view>
+							<uni-list-item showSwitch :switchChecked="itemSelected" :title="item.text" :note="item.value+''"
+								:disabled="item.disable"></uni-list-item>
+						</view>
+					</template>
+				</uni-data-select>
 			</view>
 		</uni-section>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
 			return {
-				value: 0,
+				value: 1,
+				multipleValue: [1, 2, 3],
 				range: [{
-						"value": 0,
-						"text": "篮球",
-						"disable": true
-					},
-					{
-						"value": 1,
-						"text": "足球"
-					},
-					{
-						"value": 2,
-						"text": "游泳"
-					}
-				]
+					"value": 0,
+					"text": "篮球运动",
+					"disable": true
+				}, {
+					"value": 1,
+					"text": "足球运动",
+				}, {
+					"value": 2,
+					"text": "游泳健身运动"
+				}, {
+					"value": 3,
+					"text": "跑步有氧运动",
+				}, {
+					"value": 4,
+					"text": "网球运动"
+				}, {
+					"value": 5,
+					"text": "羽毛球运动"
+				}, {
+					"value": 6,
+					"text": "乒乓球运动"
+				}, {
+					"value": 7,
+					"text": "保龄球运动"
+				}]
 			}
 		},
 		methods: {
 			change(e) {
-				console.log('e:', e);
+				console.log('单选值:', e);
+			},
+			changeMultiple(e) {
+				console.log('多选值:', e);
+			},
+			open() {
+				console.log("打开选择框")
+			},
+			close() {
+				console.log("关闭选择框")
+			},
+			clear() {
+				console.log("清除选择框")
+			},
+			// 支付宝小程序上需要这样
+			sliceArray(ls) {
+				if (Array.isArray(ls)) {
+					return ls.slice(0, 2)
+				}
+				return ls
 			}
 		}
 	}
 </script>
-
 <style lang="scss">
+	$uni-secondary-color: #909399 !default;
 	.text {
 		font-size: 12px;
 		color: #666;
@@ -72,5 +152,57 @@
 
 	.uni-pb-5 {
 		padding-bottom: 10px;
+	}
+
+	.slot-content-item {
+		background-color: $uni-secondary-color;
+		padding: 2px 5px;
+		border-radius: 5px;
+		color: #fff;
+	}
+
+	.slot-box {
+		gap: 5px;
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.empty-box {
+		width: 100%;
+		height: 100px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.select-box {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.item-view {
+		padding: 8px 12px;
+		border-radius: 4px;
+		transition: background-color 0.3s ease;
+	}
+
+	.item-selected {
+		background-color: #007aff;
+		color: #fff;
+	}
+
+	.item-disabled {
+		background-color: #f5f5f5;
+		color: #999;
+		opacity: 0.6;
+	}
+
+	.item-disabled.item-selected {
+		background-color: #d0d0d0;
+		color: #666;
 	}
 </style>
--- uni_modules/uni-data-select/components/uni-data-select/uni-data-select.vue
+++ uni_modules/uni-data-select/components/uni-data-select/uni-data-select.vue
@@ -2,30 +2,59 @@
 	<view class="uni-stat__select">
 		<span v-if="label" class="uni-label-text hide-on-phone">{{label + '：'}}</span>
 		<view class="uni-stat-box" :class="{'uni-stat__actived': current}">
-			<view class="uni-select" :class="{'uni-select--disabled':disabled}">
-				<view class="uni-select__input-box" @click="toggleSelector">
-					<view v-if="current" class="uni-select__input-text">{{textShow}}</view>
-					<view v-else class="uni-select__input-text uni-select__input-placeholder">{{typePlaceholder}}</view>
-					<view v-if="current && clear && !disabled" @click.stop="clearVal">
+			<view class="uni-select" :class="{'uni-select--disabled':disabled, 'uni-select--wrap': shouldWrap , 'border-default': mode == 'default','border-bottom': mode == 'underline'}">
+				<view class="uni-select__input-box" @click="toggleSelector" :class="{'uni-select__input-box--wrap': shouldWrap}">
+          <view v-if="slotSelected" class="slot-content padding-top-bottom" :class="{'uni-select__input-text--wrap': shouldWrap}">
+            <slot name="selected" :selectedItems="getSelectedItems()"></slot>
+          </view>
+          <template v-else>
+            <view v-if="textShow" class="uni-select__input-text" :class="{'uni-select__input-text--wrap': shouldWrap}">
+              <view class="padding-top-bottom" :class="'align-'+align">{{textShow}}</view>
+            </view>
+            <view v-else class="uni-select__input-text uni-select__input-placeholder" :class="'align-'+align">{{typePlaceholder}}</view>
+          </template>
+					<view key="clear-button" v-if="!hideRight && shouldShowClear && clear && !disabled" @click.stop="clearVal">
 						<uni-icons type="clear" color="#c0c4cc" size="24" />
 					</view>
-					<view v-else>
+					<view key="arrow-button" v-else-if="!hideRight">
 						<uni-icons :type="showSelector? 'top' : 'bottom'" size="14" color="#999" />
 					</view>
 				</view>
 				<view class="uni-select--mask" v-if="showSelector" @click="toggleSelector" />
-				<view class="uni-select__selector" :style="getOffsetByPlacement" v-if="showSelector">
-					<view :class="placement=='bottom'?'uni-popper__arrow_bottom':'uni-popper__arrow_top'"></view>
-					<scroll-view scroll-y="true" class="uni-select__selector-scroll">
-						<view class="uni-select__selector-empty" v-if="mixinDatacomResData.length === 0">
-							<text>{{emptyTips}}</text>
-						</view>
-						<view v-else class="uni-select__selector-item" v-for="(item,index) in mixinDatacomResData" :key="index"
-							@click="change(item)">
-							<text :class="{'uni-select__selector__disabled': item.disable}">{{formatItemName(item)}}</text>
-						</view>
-					</scroll-view>
-				</view>
+					<view class="uni-select__selector" :style="getOffsetByPlacement" v-if="showSelector">
+						<view :class="placement=='bottom'?'uni-popper__arrow_bottom':'uni-popper__arrow_top'"></view>
+						<scroll-view scroll-y="true" class="uni-select__selector-scroll">
+							<template v-if="slotEmpty && mixinDatacomResData.length === 0">
+								<view class="uni-select__selector-empty">
+									<slot name="empty" :empty="emptyTips"></slot>
+								</view>
+							</template>
+							<template v-else>
+								<view v-if="mixinDatacomResData.length === 0" class="uni-select__selector-empty">
+									<text>{{emptyTips}}</text>
+								</view>
+							</template>
+							<template v-if="slotOption">
+								<view v-for="(itemData,index) in mixinDatacomResData" :key="index" @click="change(itemData)">
+									<slot name="option" :item="itemData" :itemSelected="multiple? getCurrentValues().includes(itemData.value):getCurrentValues() == itemData.value"></slot>
+								</view>
+							</template>
+							<template v-else>
+								<view v-if="!multiple && mixinDatacomResData.length > 0" class="uni-select__selector-item" v-for="(item,index) in mixinDatacomResData" :key="index"
+									@click="change(item)">
+									<text :class="{'uni-select__selector__disabled': item.disable}">{{formatItemName(item)}}</text>
+								</view>
+								<view v-if="multiple && mixinDatacomResData.length > 0" >
+									<checkbox-group @change="checkBoxChange">
+										<label class="uni-select__selector-item" v-for="(item,index) in mixinDatacomResData" :key="index" >
+											<checkbox :value="index+''" :checked="getCurrentValues().includes(item.value)" :disabled="item.disable"></checkbox>
+											<view :class="{'uni-select__selector__disabled': item.disable}">{{formatItemName(item)}}</view>
+										</label>
+									</checkbox-group>
+								</view>
+							</template>
+						</scroll-view>
+					</view>
 			</view>
 		</view>
 	</view>
@@ -36,22 +65,56 @@
 	 * DataChecklist 数据选择器
 	 * @description 通过数据渲染的下拉框组件
 	 * @tutorial https://uniapp.dcloud.io/component/uniui/uni-data-select
-	 * @property {String} value 默认值
+	 * @property {String|Array} value 默认值，多选时为数组
 	 * @property {Array} localdata 本地数据 ，格式 [{text:'',value:''}]
 	 * @property {Boolean} clear 是否可以清空已选项
 	 * @property {Boolean} emptyText 没有数据时显示的文字 ，本地数据无效
 	 * @property {String} label 左侧标题
 	 * @property {String} placeholder 输入框的提示文字
 	 * @property {Boolean} disabled 是否禁用
+	 * @property {Boolean} multiple 是否多选模式
+	 * @property {Boolean} wrap 是否允许选中文本换行显示
 	 * @property {String} placement 弹出位置
 	 * 	@value top   		顶部弹出
 	 * 	@value bottom		底部弹出（default)
+	 * @property {String} align 选择文字的位置
+	 *  @value left 显示左侧
+	 *  @value center 显示中间
+	 *  @value right 显示 右侧
+	 * @property {Boolean} hideRight 是否隐藏右侧按钮
+	 * @property {String} mode 边框样式
+	 *  @value default 四周边框
+	 *  @value underline 下边框
+	 *  @value none 无边框
 	 * @event {Function} change  选中发生变化触发
+	 * @event {Function} open  选择框开启时触发
+	 * @event {Function} close  选择框关闭时触发
+	 * @event {Function} clear  点击清除按钮之后触发
 	 */
 
 	export default {
 		name: "uni-data-select",
 		mixins: [uniCloud.mixinDatacom || {}],
+		emits: [
+			'open',
+			'close',
+			'update:modelValue',
+			'input',
+			'clear',
+			'change'
+		],
+		model: {
+			prop: 'modelValue',
+			event: 'update:modelValue'
+		},
+		options: {
+			// #ifdef MP-TOUTIAO
+			virtualHost: false,
+			// #endif
+			// #ifndef MP-TOUTIAO
+			virtualHost: true
+			// #endif
+		},
 		props: {
 			localdata: {
 				type: Array,
@@ -60,11 +123,11 @@
 				}
 			},
 			value: {
-				type: [String, Number],
+				type: [String, Number, Array],
 				default: ''
 			},
 			modelValue: {
-				type: [String, Number],
+				type: [String, Number, Array],
 				default: ''
 			},
 			label: {
@@ -99,7 +162,27 @@
 			placement: {
 				type: String,
 				default: 'bottom'
-			}
+			},
+      multiple: {
+				type: Boolean,
+				default: false
+			},
+			wrap: {
+				type: Boolean,
+				default: false
+			},
+			align:{
+				type: String,
+				default: "left"
+			},
+			hideRight: {
+				type: Boolean,
+				default: false
+			},
+      mode:{
+        type: String,
+        default: 'default'
+      }
 		},
 		data() {
 			return {
@@ -133,20 +216,35 @@
 					common
 			},
 			valueCom() {
-				// #ifdef VUE3
-				return this.modelValue;
-				// #endif
-				// #ifndef VUE3
-				return this.value;
-				// #endif
+        if (this.value === '') return this.modelValue
+				if (this.modelValue === '') return this.value
+				return this.value
 			},
 			textShow() {
 				// 长文本显示
-				let text = this.current;
-				if (text.length > 10) {
-					return text.slice(0, 25) + '...';
+				if (this.multiple) {
+					const currentValues = this.getCurrentValues();
+					if (Array.isArray(currentValues) && currentValues.length > 0) {
+						const selectedItems = this.mixinDatacomResData.filter(item => currentValues.includes(item.value));
+						return selectedItems.map(item => this.formatItemName(item)).join(', ');
+					} else {
+						return ''; // 空数组时返回空字符串，显示占位符
+					}
+				} else {
+					return this.current;
 				}
-				return text;
+			},
+			shouldShowClear() {
+				if (this.multiple) {
+					const currentValues = this.getCurrentValues();
+					return Array.isArray(currentValues) && currentValues.length > 0;
+				} else {
+					return !!this.current;
+				}
+			},
+			shouldWrap() {
+				// 只有在多选模式、开启换行、且有内容时才应用换行样式
+				return this.multiple && this.wrap && !!this.textShow;
 			},
 			getOffsetByPlacement() {
 				switch (this.placement) {
@@ -155,10 +253,38 @@
 					case 'bottom':
 						return "top:calc(100% + 12px);";
 				}
+			},
+      slotSelected(){
+        // #ifdef VUE2
+        return this.$scopedSlots ? this.$scopedSlots.selected : false
+        // #endif
+        // #ifdef VUE3
+        return this.$slots ? this.$slots.selected : false
+        // #endif
+      },
+      slotEmpty(){
+        // #ifdef VUE2
+        return this.$scopedSlots ? this.$scopedSlots.empty : false
+        // #endif
+        // #ifdef VUE3
+        return this.$slots ? this.$slots.empty : false
+        // #endif
+      },
+			slotOption(){
+				// #ifdef VUE2
+				return this.$scopedSlots ? this.$scopedSlots.option : false
+				// #endif
+				// #ifdef VUE3
+				return this.$slots ? this.$slots.option : false
+				// #endif
 			}
 		},
-
 		watch: {
+			showSelector:{
+				handler(val,old){
+					val ? this.$emit('open') : this.$emit('close')
+				}
+			},
 			localdata: {
 				immediate: true,
 				handler(val, old) {
@@ -178,9 +304,20 @@
 					}
 				}
 			},
-
 		},
 		methods: {
+			getSelectedItems() {
+				const currentValues = this.getCurrentValues();
+				let _minxData = this.mixinDatacomResData
+				// #ifdef MP-WEIXIN || MP-TOUTIAO
+				_minxData = JSON.parse(JSON.stringify(this.mixinDatacomResData))
+				// #endif
+				if (this.multiple) {
+					return _minxData.filter(item => currentValues.includes(item.value)) || [];
+				} else {
+					return _minxData.filter(item => item.value === currentValues) || [];
+				}
+			},
 			debounce(fn, time = 100) {
 				let timer = null
 				return function(...args) {
@@ -188,6 +325,23 @@
 					timer = setTimeout(() => {
 						fn.apply(this, args)
 					}, time)
+				}
+			},
+			// 检查项目是否已选中
+			isSelected(item) {
+				if (this.multiple) {
+					const currentValues = this.getCurrentValues();
+					return Array.isArray(currentValues) && currentValues.includes(item.value);
+				} else {
+					return this.getCurrentValues() === item.value;
+				}
+			},
+			// 获取当前选中的值
+			getCurrentValues() {
+				if (this.multiple) {
+					return Array.isArray(this.valueCom) ? this.valueCom : (this.valueCom ? [this.valueCom] : []);
+				} else {
+					return this.valueCom;
 				}
 			},
 			// 执行数据库查询
@@ -201,7 +355,7 @@
 				}
 			},
 			initDefVal() {
-				let defValue = ''
+				let defValue = this.multiple ? [] : ''
 				if ((this.valueCom || this.valueCom === 0) && !this.isDisabled(this.valueCom)) {
 					defValue = this.valueCom
 				} else {
@@ -212,47 +366,105 @@
 					if (strogeValue || strogeValue === 0) {
 						defValue = strogeValue
 					} else {
-						let defItem = ''
+						let defItem = this.multiple ? [] : ''
 						if (this.defItem > 0 && this.defItem <= this.mixinDatacomResData.length) {
-							defItem = this.mixinDatacomResData[this.defItem - 1].value
+							defItem = this.multiple ? [this.mixinDatacomResData[this.defItem - 1].value] : this.mixinDatacomResData[this.defItem - 1].value
 						}
 						defValue = defItem
 					}
-					if (defValue || defValue === 0) {
+					if (defValue || defValue === 0 || (this.multiple && Array.isArray(defValue) && defValue.length > 0)) {
 						this.emit(defValue)
 					}
 				}
-				const def = this.mixinDatacomResData.find(item => item.value === defValue)
-				this.current = def ? this.formatItemName(def) : ''
+
+				if (this.multiple) {
+					const selectedValues = Array.isArray(defValue) ? defValue : (defValue ? [defValue] : []);
+					const selectedItems = this.mixinDatacomResData.filter(item => selectedValues.includes(item.value));
+					this.current = selectedItems.map(item => this.formatItemName(item));
+				} else {
+					const def = this.mixinDatacomResData.find(item => item.value === defValue)
+					this.current = def ? this.formatItemName(def) : ''
+				}
 			},
 
 			/**
-			 * @param {[String, Number]} value
+			 * @param {[String, Number, Array]} value
 			 * 判断用户给的 value 是否同时为禁用状态
 			 */
 			isDisabled(value) {
-				let isDisabled = false;
-
-				this.mixinDatacomResData.forEach(item => {
-					if (item.value === value) {
-						isDisabled = item.disable
-					}
-				})
-
-				return isDisabled;
+				if (Array.isArray(value)) {
+					// 对于数组，如果任意一个值被禁用，则认为整体被禁用
+					return value.some(val => {
+						return this.mixinDatacomResData.some(item => item.value === val && item.disable);
+					});
+				} else {
+					let isDisabled = false;
+					this.mixinDatacomResData.forEach(item => {
+						if (item.value === value) {
+							isDisabled = item.disable
+						}
+					})
+					return isDisabled;
+				}
 			},
-
 			clearVal() {
-				this.emit('')
+				const emptyValue = this.multiple ? [] : '';
+				this.emit(emptyValue)
+				this.current = this.multiple ? [] : ''
 				if (this.collection) {
 					this.removeCache()
 				}
+				this.$emit('clear')
+			},
+			checkBoxChange(res){
+				let range = res.detail.value
+
+				let currentValues = range && range.length > 0? range.map((item)=>{
+					const index = parseInt(item, 10);
+
+					if (isNaN(index)) {
+						console.error(`无效索引: ${item}`);
+					}
+
+					if (index < 0 || index >= this.mixinDatacomResData.length) {
+						console.error(`索引越界: ${index}`);
+					}
+
+					return this.mixinDatacomResData[index].value;
+				}) : []
+				const selectedItems = this.mixinDatacomResData.filter(dataItem => currentValues.includes(dataItem.value));
+				this.current = selectedItems.map(dataItem => this.formatItemName(dataItem));
+
+				this.emit(currentValues);
 			},
 			change(item) {
 				if (!item.disable) {
-					this.showSelector = false
-					this.current = this.formatItemName(item)
-					this.emit(item.value)
+					if (this.multiple) {
+						// 多选模式
+						let currentValues = this.getCurrentValues();
+						if (!Array.isArray(currentValues)) {
+							currentValues = currentValues ? [currentValues] : [];
+						}
+
+						const itemValue = item.value;
+						const index = currentValues.indexOf(itemValue);
+
+						if (index > -1) {
+							currentValues.splice(index, 1);
+						} else {
+							currentValues.push(itemValue);
+						}
+
+						const selectedItems = this.mixinDatacomResData.filter(dataItem => currentValues.includes(dataItem.value));
+						this.current = selectedItems.map(dataItem => this.formatItemName(dataItem));
+
+						this.emit(currentValues);
+					} else {
+						// 单选模式
+						this.showSelector = false
+						this.current = this.formatItemName(item)
+						this.emit(item.value)
+					}
 				}
 			},
 			emit(val) {
@@ -330,6 +542,11 @@
 	$uni-main-color: #333 !default;
 	$uni-secondary-color: #909399 !default;
 	$uni-border-3: #e5e5e5;
+  $uni-primary: #2979ff !default;
+	$uni-success: #4cd964 !default;
+	$uni-warning: #f0ad4e !default;
+	$uni-error: #dd524d !default;
+	$uni-info: #909399 !default;
 
 	/* #ifndef APP-NVUE */
 	@media screen and (max-width: 500px) {
@@ -352,6 +569,7 @@
 	}
 
 	.uni-stat-box {
+		background-color: #fff;
 		width: 100%;
 		flex: 1;
 	}
@@ -370,9 +588,16 @@
 		margin-right: 5px;
 	}
 
+  .border-bottom {
+    border-bottom: solid 1px $uni-border-3;
+  }
+
+  .border-default {
+    border: 1px solid $uni-border-3;
+  }
+
 	.uni-select {
 		font-size: 14px;
-		border: 1px solid $uni-border-3;
 		box-sizing: border-box;
 		border-radius: 4px;
 		padding: 0 5px;
@@ -384,14 +609,19 @@
 		/* #endif */
 		flex-direction: row;
 		align-items: center;
-		border-bottom: solid 1px $uni-border-3;
 		width: 100%;
 		flex: 1;
-		height: 35px;
+		min-height: 35px;
 
 		&--disabled {
 			background-color: #f5f7fa;
 			cursor: not-allowed;
+		}
+
+		&--wrap {
+			height: auto;
+			min-height: 35px;
+			// align-items: flex-start;
 		}
 	}
 
@@ -404,7 +634,8 @@
 	}
 
 	.uni-select__input-box {
-		height: 35px;
+		// height: 35px;
+		width: 0px;
 		position: relative;
 		/* #ifndef APP-NVUE */
 		display: flex;
@@ -412,6 +643,24 @@
 		flex: 1;
 		flex-direction: row;
 		align-items: center;
+
+		&--wrap {
+			.uni-select__input-text {
+				margin-right: 8px;
+			}
+		}
+
+    .padding-top-bottom {
+      padding-top: 5px;
+      padding-bottom: 5px;
+    }
+
+    .slot-content {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+			flex-wrap: wrap;
+    }
 	}
 
 	.uni-select__input {
@@ -463,15 +712,18 @@
 		display: flex;
 		cursor: pointer;
 		/* #endif */
+		flex-direction: row;
+		align-items: center;
 		line-height: 35px;
 		font-size: 14px;
-		text-align: center;
 		/* border-bottom: solid 1px $uni-border-3; */
 		padding: 0px 10px;
 	}
 
-	.uni-select__selector-item:hover {
-		background-color: #f9f9f9;
+
+
+	.uni-select__selector-item-check {
+		margin-left: auto;
 	}
 
 	.uni-select__selector-empty:last-child,
@@ -490,15 +742,14 @@
 	.uni-popper__arrow_bottom,
 	.uni-popper__arrow_bottom::after,
 	.uni-popper__arrow_top,
-	.uni-popper__arrow_top::after,
-	{
-	position: absolute;
-	display: block;
-	width: 0;
-	height: 0;
-	border-color: transparent;
-	border-style: solid;
-	border-width: 6px;
+	.uni-popper__arrow_top::after {
+		position: absolute;
+		display: block;
+		width: 0;
+		height: 0;
+		border-color: transparent;
+		border-style: solid;
+		border-width: 6px;
 	}
 
 	.uni-popper__arrow_bottom {
@@ -544,11 +795,22 @@
 		text-overflow: ellipsis;
 		-o-text-overflow: ellipsis;
 		overflow: hidden;
+
+		&--wrap {
+			white-space: normal;
+			text-overflow: initial;
+			-o-text-overflow: initial;
+			overflow: visible;
+			word-wrap: break-word;
+			word-break: break-all;
+			// line-height: 1.5;
+		}
 	}
 
 	.uni-select__input-placeholder {
 		color: $uni-base-color;
 		font-size: 12px;
+    margin: 1px 0;
 	}
 
 	.uni-select--mask {
@@ -559,4 +821,17 @@
 		left: 0;
 		z-index: 2;
 	}
+
+  .align-left {
+    text-align: left;
+  }
+
+  .align-center {
+    text-align: center;
+  }
+
+  .align-right {
+    text-align: right;
+  }
+
 </style>
