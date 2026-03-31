2322dc366db4641748d93c3180a02182dadc0792
DCloud_LXH <283700113@qq.com>
2026-03-31 13:06:42 +0800

    feat(web-view): 添加 issue-27817 测试例 #27817

---
+++ pages/component/web-view/issue-27817.test.js
@@ -0,0 +1,29 @@
+const platformInfo = process.env.uniTestPlatformInfo.toLocaleLowerCase()
+const isAndroid = platformInfo.startsWith('android')
+const isIOS = platformInfo.startsWith('ios')
+const isHarmony = platformInfo.startsWith('harmony')
+const isAppWebView = process.env.UNI_AUTOMATOR_APP_WEBVIEW == 'true'
+const isDom2 = process.env.UNI_APP_X_DOM2 === "true"
+
+describe('web-view', () => {
+  if (!isHarmony) {
+    it('not support', () => {
+      expect(1).toBe(1)
+    })
+    return
+  }
+
+  let page
+  beforeAll(async () => {
+    page = await program.reLaunch('/pages/component/web-view/issue-27817')
+    await page.waitFor('view')
+    await page.waitFor(1000)
+  })
+
+  it('src test', async () => {
+    const image = await program.screenshot({
+      fullPage: true
+    });
+    expect(image).toSaveImageSnapshot();
+  })
+})
